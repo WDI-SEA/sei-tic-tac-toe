331b45ce28e4d24478e111328b8b7cfa9c8a5b0a
wkoslik <wkoslik@gmail.com>
2020-12-18 21:09:14 -0500

    psuedo code and basic html

---
+++ index.js
@@ -0,0 +1,16 @@
+/* JS psuedo code */
+
+/*
+
+To do: (in no particular order)
+
+1. Check for a win condition
+2. when player clicks, add a class to that div
+3. when a player clicks, check to make sure that div wasn't already selected
+4. when a player clicks a previously unselected div, remove :hover from div
+5. After every player click, check for a win condition
+6. If a win hasn't happened, and there are no "empty" grid spaces, tie condition is met
+7. If win condition hasn't happened && there are empty squares, game continues
+8. Reset button
+9. local storage? 
+10. keep score

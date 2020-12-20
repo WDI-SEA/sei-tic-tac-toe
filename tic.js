e8caa6c6797bdf6b1fe91eb640dff0b742ee241d
JJDev <dancesfire@gmail.com>
2020-12-19 19:20:18 -0800

    Html and css versoin1.1 java script V.#0!

---
+++ tic.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+});
+
+// const newLocal = document.getElementByClass("a1") function() {style.color = "blue"; 
+// }
+let aic = document.querySelector(".a1")
+a1.style.backgroundColor= 'black';
+// .onclick = function () {
+    console.log("click");
+    // .a1
+  
+// document.getElementsByClassName("a1").onclick.a1.innerHTML="X!!S";
+// ()=> {document.getElementByClass("a1").src = "http://placekitten.com/g/200/300"; 
+    // }}    
+
+// function ShowUndo() 
+// {
+//     document.getElementById("image").src = "http://www.userinterfaceicons.com/80x80/undo.png"; 
+// }   
+    // document.querySelector(".a1").style.transform.background-color = red;
+    
+
+    //your code here
+    
+
+// addEventListener("onClick", () =>  {
+//     console.log("clickmore")
+// });

4b23433117b84f9cb8b9bd0039e7868525301b09
Vince Hintermeister <vv.hintermeister@gmail.com>
2020-12-22 09:47:04 -0600

    got display box up

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var gridContainer = document.getElementById("gridContainer")
 
 gridContainer.addEventListener("click", click)
 
-var oMarker = "https://toppng.com/free-image/wooden-capital-letter-o-PNG-free-PNG-Images_119275" 
+var oMarker = "https://cdn.pixabay.com/photo/2017/02/09/11/25/alphabet-2051685_960_720.png"
 
-var xMarker = "https://www.nicepng.com/png/full/205-2057069_letter-x-with-a-transparent-background.png"
+var xMarker = "https://cdn.pixabay.com/photo/2017/02/09/11/09/alphabet-2051646_960_720.png"
 
 
 
@@ -17,6 +17,10 @@ function changeSize() {
     backgroundImage.style.height = "50px";
     backgroundImage.style.width = "50px";
 }
+
+
+
+
 
 function click(event) {
     if (whosTurn === "X") {
@@ -43,6 +47,37 @@ function click(event) {
         }
     }  
 }
+
+function checkWin() {
+    if (gameBoard[0] === "xClicked" && gameBoard[1] === "xClicked" && gameBoard[2] === "xClicked" ||
+    gameBoard[3] === "xClicked" && gameBoard[4] === "xClicked" && gameBoard[5] === "xClicked" ||
+    gameBoard[6] === "xClicked" && gameBoard[7] === "xClicked" && gameBoard[8] === "xClicked" ||
+    gameBoard[0] === "xClicked" && gameBoard[3] === "xClicked" && gameBoard[6] === "xClicked" ||
+    gameBoard[1] === "xClicked" && gameBoard[4] === "xClicked" && gameBoard[7] === "xClicked" ||
+    gameBoard[2] === "xClicked" && gameBoard[5] === "xClicked" && gameBoard[8] === "xClicked" ||
+    gameBoard[0] === "xClicked" && gameBoard[4] === "xClicked" && gameBoard[8] === "xClicked" ||
+    gameBoard[2] === "xClicked" && gameBoard[4] === "xClicked" && gameBoard[6] === "xClicked" 
+    ) {
+// add o
+//add cats 
+        DataView.innerText = "X WINS!"
+        console.log("X Wins!")
+
+    } else if (gameBoard[0] === "oClicked" && gameBoard[1] === "oClicked" && gameBoard[2] === "oClicked" ||
+    gameBoard[3] === "oClicked" && gameBoard[4] === "oClicked" && gameBoard[5] === "oClicked" ||
+    gameBoard[6] === "oClicked" && gameBoard[7] === "oClicked" && gameBoard[8] === "oClicked" ||
+    gameBoard[0] === "oClicked" && gameBoard[3] === "oClicked" && gameBoard[6] === "oClicked" ||
+    gameBoard[1] === "oClicked" && gameBoard[4] === "oClicked" && gameBoard[7] === "oClicked" ||
+    gameBoard[2] === "oClicked" && gameBoard[5] === "oClicked" && gameBoard[8] === "oClicked" ||
+    gameBoard[0] === "oClicked" && gameBoard[4] === "oClicked" && gameBoard[8] === "oClicked" ||
+    gameBoard[2] === "oClicked" && gameBoard[4] === "oClicked" && gameBoard[6] === "oClicked" 
+    ) {
+        console.log("O Wins!")
+    } else if (gameboard[0, 1, 2, 3, 4, 5, 6, 7, 8] === clickedOn ) {
+        console.log("Cat's Game!")
+    }
+}
+
 
 
 

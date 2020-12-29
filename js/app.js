6ba30273d11a96fce9032bffc1c25e80b0d7b56c
kys3923 <kys3923@gmail.com>
2020-12-29 01:56:23 -0500

    completed

--- js/app.js
+++ js/app.js
@@ -15,164 +15,124 @@ const classNames = {
 
 let cValues = ["", "", "", "", "", "", "", "", ""];
 let nextTurn = true;
-let turnsLeft = 9;
-const winningPattern = [
-        [0, 1, 2], // 0
-        [3, 4, 5], // 1
-        [6, 7, 8], // 2
-        [0, 3, 6], // 3
-        [1, 4, 7], // 4
-        [2, 5, 8], // 5
-        [0, 4, 8], // 6
-        [2, 4, 6] // 7
-];
-const dependPattern = [
-        [0, 1], //0, 2  // depend cell number and variable number
-        [1, 2], //1, 0
-        [3, 4], //2, 5
-        [4, 5], //3, 3
-        [6, 7], //4, 8
-        [7, 8], //5, 9
-        [0, 3], //6, 6
-        [3, 6], //7, 0
-        [1, 4], //8, 7
-        [4, 7], //9, 1
-        [2, 5], //10, 8
-        [5, 8], //11, 2
-        [0, 4], //12, 8
-        [4, 8], //13, 0
-        [2, 4], //14, 6
-        [4, 6], //15, 2
-        [0, 2], //16, 1
-        [3, 5], //17, 4
-        [6, 8], //18, 7
-        [0, 6], //19, 3
-        [1, 7], //20, 4
-        [2, 8], //21, 5
-        [0, 8], //22, 4
-        [2, 6], //23, 4
-];
+const box1 = document.getElementById("box1");
+const box2 = document.getElementById("box2");
+const box3 = document.getElementById("box3");
+const box4 = document.getElementById("box4");
+const box5 = document.getElementById("box5");
+const box6 = document.getElementById("box6");
+const box7 = document.getElementById("box7");
+const box8 = document.getElementById("box8");
+const box9 = document.getElementById("box9");
 
 // game options
-function gameSelector() => {
+function gameSelector() {
     if (document.getElementById("human").checked && document.getElementById("StartO").checked) {
-        let gameActive = true;
-        let currentPlayer = "O";
+        gamePlay1();
     }
     else if (document.getElementById("human").checked && document.getElementById("StartX").checked) {
         let gameActive = true;
         let currentPlayer = "X";
-        computerPlacement();
-    }
-    else if (document.getElementById("comput").checked && document.getElementById("StartO").checked) {
-        let gameActive = true;
-        let currentPlayer = "O";
-    }
-    else if (document.getElementById("comput").checked && document.getElementById("StartX").checked) {
-        let gameActive = true;
-        let currentPlayer = "X";
-        computerPlacement();
+        gamePlay2();
     }
     else {
-        messageOut.innerHTML = "Choose your game type"
+        messageOut.innerHTML = "Going First or Second?"
     }
-        
 };
 
 
-function humanPlacement () => {
+// Vs human, start O
 
+function gamePlay1() {
+    let boxes = cells, insideText = 0;
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].onclick = function() {
+            if(this.innerHTML !== "X" && this.innerHTML !== "O" && this.innerHTML !== " ") {
+                if(insideText%2 === 0) {
+                    this.innerHTML = "O";
+                    this.style.backgroundImage = "url('./img/button2.png')"
+                    messageOut.innerHTML = "turn for X"
+                    insideText += 1;
+                }
+                else {
+                    this.innerHTML = "X";
+                    this.style.backgroundImage = "url('./img/button3.png')"
+                    messageOut.innerHTML = "turn for O"
+                    insideText += 1;
+                }
+            };
+            checkWin();
+        }
+    }
+};
+function gamePlay2() {
+    let boxes = cells, insideText = 0;
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].onclick = function() {
+            if(this.innerHTML !== "X" && this.innerHTML !== "O" && this.innerHTML !== " ") {
+                if(insideText%2 === 0) {
+                    this.innerHTML = "X";
+                    this.style.backgroundImage = "url('./img/button3.png')"
+                    messageOut.innerHTML = "turn for O"
+                    insideText += 1;
+                }
+                else {
+                    this.innerHTML = "O";
+                    this.style.backgroundImage = "url('./img/button2.png')"
+                    messageOut.innerHTML = "turn for X"
+                    insideText += 1;
+                }
+            };
+            checkWin();
+        }
+    }
 };
 
-//     // prevents refresh on buttons
-//     document.querySelector(".startbtn").addEventListener("click", (e) => {
-//         e.preventDefault();
 
-//         // if condition1 game selectd
-//         if (document.getElementById("human").checked && document.getElementById("StartO").checked) {
-            
-//             let gameActive = true;
-//             var currentPlayer = "O";
+function checkWin() {
+    if(box1.innerHTML === "X" && box2.innerHTML === "X" && box3.innerHTML === "X" ||
+        box4.innerHTML === "X" && box5.innerHTML === "X" && box6.innerHTML === "X" ||
+        box7.innerHTML === "X" && box8.innerHTML === "X" && box9.innerHTML === "X" ||
+        box1.innerHTML === "X" && box4.innerHTML === "X" && box7.innerHTML === "X" ||
+        box2.innerHTML === "X" && box5.innerHTML === "X" && box8.innerHTML === "X" ||
+        box3.innerHTML === "X" && box6.innerHTML === "X" && box9.innerHTML === "X" ||
+        box3.innerHTML === "X" && box5.innerHTML === "X" && box7.innerHTML === "X" ||
+        box1.innerHTML === "X" && box5.innerHTML === "X" && box9.innerHTML === "X") {
+            messageOut.innerHTML = "Winner is X"
+            disableGame();
+        }
+    else if(
+        box1.innerHTML === "O" && box2.innerHTML === "O" && box3.innerHTML === "O" ||
+        box4.innerHTML === "O" && box5.innerHTML === "O" && box6.innerHTML === "O" ||
+        box7.innerHTML === "O" && box8.innerHTML === "O" && box9.innerHTML === "O" ||
+        box1.innerHTML === "O" && box4.innerHTML === "O" && box7.innerHTML === "O" ||
+        box2.innerHTML === "O" && box5.innerHTML === "O" && box8.innerHTML === "O" ||
+        box3.innerHTML === "O" && box6.innerHTML === "O" && box9.innerHTML === "O" ||
+        box3.innerHTML === "O" && box5.innerHTML === "O" && box7.innerHTML === "O" ||
+        box1.innerHTML === "O" && box5.innerHTML === "O" && box9.innerHTML === "O") {
+            messageOut.innerHTML = "Winner is O"
+            disableGame();
+        }
+    else if(
+        box1.innerText != "" && box2.innerText != "" && box3.innerText != "" &&
+        box4.innerText != "" && box5.innerText != "" && box6.innerText != "" &&
+        box7.innerText != "" && box8.innerText != "" && box9.innerText != "") {
+            messageOut.innerHTML = "Draw Game"
+        }
+}
 
-//             // checks for win
-//             const checkWin = (chosenIndex) => {
-//                 const winningRanges = winningPattern[chosenIndex];
-                
-//                 for (let i = 0; i < winningRanges; i++) {
-//                     const currentEntry = cValues[e.target.value];
-//                     const winOption1 = cValues[winningPattern[i][0]];
-//                     const winOption2 = cValues[winningPattern[i][1]];
-//                     const winOption3 = cValues[winningPattern[i][2]];
 
-//                     if (winOption1 === winOption2 && winOption2 === winOption3) {
-//                        winningPlayer = currentEntry;
-//                        return gameActive=false;
-//                     };
-//                 };
 
-//                 if (turnsLeft === 0) {
-//                     winningPlayer = "draw";
-//                     return gameActive = false;
-//                 }
-                
-//                 return true;
-//             };
-//             const winnerWrite = () => {
-//                 if (winningPlayer === "draw") {
-//                     messageOut.innerHTML = "Draw Game";
-//                 } else {
-//                     messageOut.innerHTML = `${winningPlayer} wins!`;
-//                 };
-//             };
+function disableGame() {
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].onclick = function() {
+            return false;
+        }
+    }
+}
 
-//             cells.forEach((a, i) => {
 
-//                 a.addEventListener("click", () => {
-                    
-//                     // puts different text each click
-//                     if (!cValues[i]) {
-//                         cValues[i] = nextTurn ? player.o : player.x;
-//                         nextTurn = !nextTurn;
-//                         turnsLeft--;
-
-//                         if (winningPlayer) {
-//                             winnerWrite();
-//                         }
-                        
-//                         const fillIn = a.querySelector(`.${classNames.cellText}`);
-//                         fillIn.innerHTML = cValues[i];
-                        
-//                     };
-                    
-
-//                 });
-//             }); 
-
-//             winnerWrite();
-//         };
-        
-//     });
-
-    
-// });
-    // list of functions
-    // start game
-    //     check game status 
-    //     depend and fill
-    //     win or draw (o, x)
-    //     checking the box
-    //     filling the box
-    //     algorithms
-    //         if vsHuman, -> humanO or humanX -> check for win -> if win or draw, reset
-    //         else 
-    //         compuO -> fill cell5 -> wait for event -> check depend -> fill -> if win or draw, reset  
-    //         compuX -> wait for event -> check depend -> fill -> if win or draw, reset
-    //     output text if win or draw
-    //     reset
-
-       // Game selections
-
-    // condition1 = document.getElementById("human").checked && document.getElementById("StartO").checked;
-    // condition2 = document.getElementById("human").checked && document.getElementById("StartX").checked;
-    // condition3 = document.getElementById("ai").checked && document.getElementById("StartO").checked;
-    // condition4 = document.getElementById("ai").checked && document.getElementById("StartX").checked;
+document.getElementById("clickA").addEventListener("click", (event) => {
+    event.preventDefault();
+    gameSelector();
+});

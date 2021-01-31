f92ea74f53d2f69acba95f744629aec3c846ac24
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-01-31 15:26:18 -0800

    css and html and start of js

---
+++ tic-tac-toe.js
@@ -0,0 +1,8 @@
+console.log('hi from js')
+let activePlayer = "O"
+let gamePlay = true
+const displayGameStatus = document.querySelector('.game-status')
+let moves = ['', '', '', '', '', '', '', '', '']
+const playerWins = console.log('Player ' + activePlayer + ' is the winner!')
+const catsGame = console.log(`It's a tie!`)
+const whosTurn = console.log(activePlayer + ', it is your turn')

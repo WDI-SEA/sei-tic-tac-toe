937263a4432a58ba41005f9d40147935273eacc5
darrengp <ceo.darrenperry@gmail.com>
2021-01-31 11:34:56 -0800

    updated checkForWin algo

--- main.js
+++ main.js
@@ -107,89 +107,24 @@ function checkForWin() {
     playWin.innerText = "wow you won!";
     play = false;
   } else if (
-    box1.innerText == "X" &&
-    box2.innerText == "X" &&
-    box3.innerText == "X"
+    (box1.innerText == "X" && box2.innerText == "X" && box3.innerText == "X") ||
+    (box1.innerText == "X" && box5.innerText == "X" && box9.innerText == "X") ||
+    (box2.innerText == "X" && box5.innerText == "X" && box8.innerText == "X") ||
+    (box3.innerText == "X" && box6.innerText == "X" && box9.innerText == "X") ||
+    (box3.innerText == "X" && box5.innerText == "X" && box7.innerText == "X") ||
+    (box4.innerText == "X" && box5.innerText == "X" && box6.innerText == "X")
   ) {
     console.log("win");
     playWin.innerText = "wow you won!";
     play = false;
   } else if (
-    box1.innerText == "X" &&
-    box5.innerText == "X" &&
-    box9.innerText == "X"
-  ) {
-    console.log("win");
-    playWin.innerText = "wow you won!";
-    play = false;
-  } else if (
-    box2.innerText == "X" &&
-    box5.innerText == "X" &&
-    box8.innerText == "X"
-  ) {
-    console.log("win");
-    playWin.innerText = "wow you won!";
-    play = false;
-  } else if (
-    box3.innerText == "X" &&
-    box6.innerText == "X" &&
-    box9.innerText == "X"
-  ) {
-    console.log("win");
-    playWin.innerText = "wow you won!";
-    play = false;
-  } else if (
-    box3.innerText == "X" &&
-    box5.innerText == "X" &&
-    box7.innerText == "X"
-  ) {
-    console.log("win");
-    playWin.innerText = "wow you won!";
-    play = false;
-  } else if (
-    box1.innerText == "O" &&
-    box4.innerText == "O" &&
-    box7.innerText == "O"
-  ) {
-    console.log("win");
-    playWin.innerText = "wow you won!";
-    play = false;
-  } else if (
-    box1.innerText == "O" &&
-    box2.innerText == "O" &&
-    box3.innerText == "O"
-  ) {
-    console.log("win");
-    playWin.innerText = "wow you won!";
-    play = false;
-  } else if (
-    box1.innerText == "O" &&
-    box5.innerText == "O" &&
-    box9.innerText == "O"
-  ) {
-    console.log("win");
-    playWin.innerText = "wow you won!";
-    play = false;
-  } else if (
-    box2.innerText == "O" &&
-    box5.innerText == "O" &&
-    box8.innerText == "O"
-  ) {
-    console.log("win");
-    playWin.innerText = "wow you won!";
-    play = false;
-  } else if (
-    box3.innerText == "O" &&
-    box6.innerText == "O" &&
-    box9.innerText == "O"
-  ) {
-    console.log("win");
-    playWin.innerText = "wow you won!";
-    play = false;
-  } else if (
-    box3.innerText == "O" &&
-    box5.innerText == "O" &&
-    box7.innerText == "O"
+    (box4.innerText == "O" && box5.innerText == "O" && box6.innerText == "O") ||
+    (box1.innerText == "O" && box4.innerText == "O" && box7.innerText == "O") ||
+    (box1.innerText == "O" && box2.innerText == "O" && box3.innerText == "O") ||
+    (box1.innerText == "O" && box5.innerText == "O" && box9.innerText == "O") ||
+    (box2.innerText == "O" && box5.innerText == "O" && box8.innerText == "O") ||
+    (box3.innerText == "O" && box6.innerText == "O" && box9.innerText == "O") ||
+    (box3.innerText == "O" && box5.innerText == "O" && box7.innerText == "O")
   ) {
     console.log("win");
     playWin.innerText = "wow you won!";

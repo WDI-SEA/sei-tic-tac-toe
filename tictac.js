dcfc31ece77b60f1eaf977ade556cfae773d1072
AlrightErin27 <erinmontybruce@gmail.com>
2021-01-30 16:33:09 -0800

    initial CSS & HTML- playing with some JS

---
+++ tictac.js
@@ -0,0 +1,24 @@
+let gamePanel = ["", "", "", "", "", "", "", "", ""];
+//////////
+const panelContainer = document.querySelector(".gridArea");
+let player = "X";
+let computer = "0";
+
+const givePanel = () => {
+    panelContainer.innerHTML = "";
+    gamePanel.forEach((e, i) => {
+        panelContainer.innerHTML += `div id="square_${i}" class="square" onclick="addPlayerMove(${i})">${gamePanel[i]}</div>`;
+        if (e == player || e == computer) {
+            document.querySelector(`#square${i}`).classList.add("occupied");
+        }
+    });
+};
+
+// givePanel();
+const addPlayerMove = e => {
+    if (gamePanel[e] == "") {
+        gamePanel[e] = player;
+        givePanel();
+        addComputerMove();
+    }
+}

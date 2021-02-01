4084976b1ff2d75bc325729e1fbc0517607aa756
ChrisB007 <chrisbitoy@gmail.com>
2021-02-01 14:59:55 -0500

    more modifications

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 // Select players.
-let X, O;
+let player1 = "X", player2 = "O";
 
 function selectPlayer(){
+    let playO = document.querySelector("#playertwo");
+    let playX = document.querySelector("#playerone").addEventListener("click", function(e){
+        console.log(e);
+    })
+    }
 
-}
+    selectPlayer();
 
 
 // Add array of Players to the game, with "X" and "O" values.
 
-
-// Add eventListener to listen for click events
+// Add eventListener to listen for click events.
 
 //Add an empty array to store the clicked "X and O" values.
 
 //Loop over the DOM elements, and randomly push the 'X and O' onClick.
 
-//Setup a reset button
+//Setup a reset button.

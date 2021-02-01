210efe9b0768f90db850ea0ebb9a1021ad510556
Jcom619 <joshuacompton@my.smccd.edu>
2021-01-31 16:01:47 -0800

    adding some js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("test");
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.box');
+
+    squares.forEach(square => {
+        square.addEventListener('click', clickOutcome);
+    });
+
+    function clickOutcome() {
+        alert('clicked')
+    }
+})

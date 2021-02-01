1b24e488e927ae3359dcd11b10ba3bea24eff9f1
gabrielbustos550 <gabrielbustos550@gmail.com>
2021-02-01 00:05:06 -0700

    80% done

--- JS/tic-tac-toe.js
+++ JS/tic-tac-toe.js
@@ -34,25 +34,19 @@ let bottomRight = document.querySelector('#bottom_right');
 
 
 
-
-
-
-
-
-
 function initializeGame() {
     gameOver = false
     spotsToFill= []
     boardState= {
-        top_left: false,
-        top_center: false,
-        top_right: false, 
-        middle_left: false,
-        middle_center: false,
-        middle_right: false, 
-        bottom_left: false,
-        bottom_center: false,
-        bottom_right: false, 
+        top_left: null,
+        top_center: null,
+        top_right: null, 
+        middle_left: null,
+        middle_center: null,
+        middle_right: null, 
+        bottom_left: null,
+        bottom_center: null,
+        bottom_right: null, 
     }
     counter = 0
 
@@ -63,20 +57,41 @@ function initializeGame() {
 
 function selectBox(event){
     let boxLocation = event.target.id;
-    if(!gameOver && boardState[boxLocation] === false){
+    if(!gameOver && boardState[boxLocation] === null){
         if(counter %2 === 0){
         event.target.innerText = 'X'
         }else{
         event.target.innerText = "O"
         }
-        boardState[boxLocation] = true;
+        boardState[boxLocation] = event.target.innerText
         counter++;
     }
+    if(boardState.top_left !== null && boardState.top_left === boardState.top_center && boardState.top_right === boardState.top_left){
+    gameOver = true
+
+    if(counter %2 === 0){
+    document.querySelector('#result').innerText= 'Game is over! O won the game!' 
+    } else{
+        document.querySelector('#result').innerText= 'Game is over! X Won the Game'
+    }}
+    
     
 }
 
-function resetGame(){
 
+function resetGame(){
+    topLeft.innerText = " "
+    topCenter.innerText = " "
+    topRight.innerText = " "
+    middleLeft.innerText = " "
+    middleCenter.innerText = " "
+    middleRight.innerText = " "
+    bottomLeft.innerText = " "
+    bottomCenter.innerText = " "
+    bottomRight.innerText = " " 
+    document.querySelector('#result').innerText= ''
+    
+    initializeGame()
 }
 
 
@@ -121,4 +136,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //if list of spots to fill is blank, the place x in there
 // if spots to fill is x, then make it an o
-// if there is a click inside of div then
+// if there is a click inside of div

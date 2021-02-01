e8d65392d8e9db0bb434c573e5be408a22baff10
ChrisB007 <chrisbitoy@gmail.com>
2021-02-01 12:15:32 -0500

    added reset button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-// Add eventListener to listen for click events
+// Select players.
+let X, O;
+
+function selectPlayer(){
+
+}
+
 
 // Add array of Players to the game, with "X" and "O" values.
+
+
+// Add eventListener to listen for click events
 
 //Add an empty array to store the clicked "X and O" values.
 

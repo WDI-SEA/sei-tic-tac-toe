d9321194260699270913246183cec22fb825885e
SunilDeveloper7 <sunnil474@gmail.com>
2021-02-01 09:57:21 -0800

    Added If/else statement on JS file

--- script.js
+++ script.js
@@ -7,60 +7,81 @@ document.addEventListener('DOMContentLoaded', (e)=>{
   },2000);
 })
 
-
-
 const statusDiv = document.querySelector('.status');
 const resetDiv = document.querySelector('.reset');
 const cellDivs = document.querySelectorAll('.game-cell');
 
-let status = function() {
-  if (statusDiv == playerFirst) {
-    statusDiv.appendChild(playerSecond)
-  }else{
-    statusDiv.appendChild(playerFirst)
-  
+// game constants
+const xSymbol = '×';
+const oSymbol = 'o';
+
+// game variables
+let gameIsLive = true;
+let xIsNext = true;
+
+
+// functions
+const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
+
+const handleWin = (letter) => {
+  gameIsLive = false;
+  if (letter === 'x') {
+    statusDiv.innerHTML = `${letterToSymbol(letter)} has won!`;
+  } else{(letter === 'o')
+    statusDiv.innerHTML = `${letterToSymbol(letter)} has won!`;
   }
-}
-
-
-
-//declared game variable
-let gameIslive=true;
-let xIsNext =true;
-
-//create a variable player
-
-let playerFirst = 'x';
-let playerSecond = 'o';
-
-//event Handler
-
-const handleReset =(e)=> {
-  console.log(e);
-
-
-}
-
-
-const handleCellClick =(e)=> {
-
-  const location =e.target.classList[1];
-
-if (xIsNext) {
-  e.target.classList.add('x');
-  xIsNext =!xIsNext;
-}else {
-  e.target.classList.add('o');
-  xIsNext =!xIsNext;
-}
-
-  
 };
 
+const checkGameStatus = () => {
+  const topLeft = cellDivs[0].classList[1];
+  const topMiddle = cellDivs[1].classList[1];
+  const topRight = cellDivs[2].classList[1];
+  const middleLeft = cellDivs[3].classList[1];
+  const middleMiddle = cellDivs[4].classList[1];
+  const middleRight = cellDivs[5].classList[1];
+  const bottomLeft = cellDivs[6].classList[1];
+  const bottomMiddle = cellDivs[7].classList[1];
+  const bottomRight = cellDivs[8].classList[1];
 
-//event Listeners
-resetDiv.addEventListener('click', handleReset);
-
-for(const cellDiv of cellDivs) {
-cellDiv.addEventListener('click', handleCellClick)
-}
+  // check winner
+  if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+    handleWin(topLeft);
+    cellDivs[0].classList.add('won');
+    cellDivs[1].classList.add('won');
+    cellDivs[2].classList.add('won');
+  } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
+    handleWin(middleLeft);
+    cellDivs[3].classList.add('won');
+    cellDivs[4].classList.add('won');
+    cellDivs[5].classList.add('won');
+  } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
+    handleWin(bottomLeft);
+    cellDivs[6].classList.add('won');
+    cellDivs[7].classList.add('won');
+    cellDivs[8].classList.add('won');
+  } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
+    handleWin(topLeft);
+    cellDivs[0].classList.add('won');
+    cellDivs[3].classList.add('won');
+    cellDivs[6].classList.add('won');
+  } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
+    handleWin(topMiddle);
+    cellDivs[1].classList.add('won');
+    cellDivs[4].classList.add('won');
+    cellDivs[7].classList.add('won');
+  } else if (topRight && topRight === middleRight && topRight === bottomRight) {
+    handleWin(topRight);
+    cellDivs[2].classList.add('won');
+    cellDivs[5].classList.add('won');
+    cellDivs[8].classList.add('won');
+  } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
+    handleWin(topLeft);
+    cellDivs[0].classList.add('won');
+    cellDivs[4].classList.add('won');
+    cellDivs[8].classList.add('won');
+  } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
+    handleWin(topRight);
+    cellDivs[2].classList.add('won');
+    cellDivs[4].classList.add('won');
+    cellDivs[6].classList.add('won');
+  

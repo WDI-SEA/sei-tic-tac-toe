e408573327b09aae6b8db732ab09b0df3ac82f30
Jcom619 <joshuacompton@my.smccd.edu>
2021-02-01 06:07:43 -0800

    one day, not today though

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const squares = document.querySelectorAll('.box')
-  const playerDisplay = document.querySelector('#player')
+    const squares = document.querySelectorAll('.box')
+    const playerDisplay = document.querySelector('#player')
 
-  let currentPlayer = 'playerX'
+    let currentPlayer = 'X'
 
-  squares.forEach(square => {
-    square.addEventListener('click', clickOutcome)
-  })
 
-  function clickOutcome(e) {
-    const squareArray = Array.from(squares)
-    const index = squareArray.indexOf(e.target)
-    playerDisplay.innerText = currentPlayer
+    squares.forEach(square => {
+        square.addEventListener('click', clickOutcome)
+    })
 
-    if(currentPlayer === 'playerX') {
-      squares[index].classList.add('playerX')
-      currentPlayer = 'playerO'
-    } else {
-      squares[index].classList.add('playerO')
-      currentPlayer = 'playerX'
+    function clickOutcome(e) {
+        const squareArray = Array.from(squares)
+        const index = squareArray.indexOf(e.target)
+        playerDisplay.innerText = currentPlayer
+
+        if(currentPlayer === 'X') {
+            squares[index].classList.add('X')
+            console.log(index)
+            currentPlayer = 'O'
+        } else {
+            squares[index].classList.add('O')
+            currentPlayer = 'X'
+        }
     }
-  }
+
 
 
 })

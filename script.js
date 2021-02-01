bae36d3138350e634998e77e6a267eb4a39eb679
Jcom619 <joshuacompton@my.smccd.edu>
2021-02-01 01:51:41 -0800

    Update script.js

--- script.js
+++ script.js
@@ -22,12 +22,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => {
-      return response.json()
-    })
-    .then(users => {
-      console.log(users)
-    })
 
 })

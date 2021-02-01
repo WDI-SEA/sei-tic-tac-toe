b5fe4662afe83873d362140c57a4ca6142598839
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-01-31 17:56:59 -0800

    kind of functional game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,82 @@
-console.log('hi from js')
-let activePlayer = "O"
-let gamePlay = true
-const displayGameStatus = document.querySelector('.game-status')
-let moves = ['', '', '', '', '', '', '', '', '']
-const playerWins = console.log('Player ' + activePlayer + ' is the winner!')
-const catsGame = console.log(`It's a tie!`)
-const whosTurn = console.log(activePlayer + ', it is your turn')
+const statusDisplay = document.querySelector('.gameStatus');
+
+let gameActive = true;
+
+let currentPlayer = "X";
+
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const win = () => `Player ${currentPlayer} has won!`;
+const catsGame = () => `Game ended in a draw!`;
+const activePlayer = () => `It's ${currentPlayer}'s turn`;
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = activePlayer();
+}
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+if (roundWon) {
+        statusDisplay.innerHTML = win();
+        gameActive = false;
+    }
+}
+
+function handleCellClick(clickedCellEvent) {
+        const clickedCell = clickedCellEvent.target;
+        const clickedCellIndex = parseInt(
+          clickedCell.getAttribute('data-cell-index')
+        );
+        if (gameState[clickedCellIndex] !== "" || !gameActive) {
+            return;
+        }   
+        handleCellPlayed(clickedCell, clickedCellIndex);
+        handleResultValidation();
+    }
+        let roundDraw = !gameState.includes("");
+        if (roundDraw) {
+            statusDisplay.innerHTML = drawMessage()
+            gameActive = false
+        }
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = activePlayer();
+    document.querySelectorAll('.cell')
+               .forEach(cell => cell.innerHTML = "");
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.gameRestart').addEventListener('click', handleRestartGame);

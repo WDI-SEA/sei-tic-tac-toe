8a8173f2e7207cc1f9b0cf74195f74cba7535ec8
ChrisB007 <chrisbitoy@gmail.com>
2021-02-01 16:04:46 -0500

    selecting players

--- js/script.js
+++ js/script.js
@@ -1,14 +1,49 @@
+
 // Select players.
-let player1 = "X", player2 = "O";
 
-function selectPlayer(){
-    let playO = document.querySelector("#playertwo");
-    let playX = document.querySelector("#playerone").addEventListener("click", function(e){
-        console.log(e);
-    })
+let player1 = document.querySelector("#playerone"), currentPlayer, player2 = document.querySelector("#playertwo");
+
+function selectPlayer() {
+    player1.addEventListener("click", function(e) {
+        if (e.target.id === "playerone") {
+            console.log(e.target)
+            player1.classList.add("buttoncolor");
+            document.querySelector("#selectplayer").textContent = `You have selected "X"`;
+        } else if (e.target.id === "playerone") {
+            console.log(e.target)
+            player1.classList.add("buttoncolor");
+            document.querySelector("#selectplayer").textContent = `You have selected "O"`;
     }
+})
+}
 
-    selectPlayer();
+selectPlayer();
+
+
+
+
+
+
+//     // if (currentPlayer === player1){
+//     //     player1.addEventListener("click", function(e){
+//     //         console.log(e.target)
+//     //         player1.classList.add("buttoncolor")
+//     //     });
+//     //     document.querySelector("#selectplayer").textContent = `You have selected "X"`;
+//     //
+//     // } else if (currentPlayer === player2){
+//     //     player2.addEventListener("click", function(e){
+//     //         console.log(e.target.id="playertwo")
+//     //         player2.classList.add("buttoncolor")
+//     //     });
+//     //
+//     //     document.querySelector("#selectplayer").textContent = `You have selected "O"`;
+//     // }
+//
+//     return currentPlayer;
+// }
+//
+// selectPlayer(player2);
 
 
 // Add array of Players to the game, with "X" and "O" values.

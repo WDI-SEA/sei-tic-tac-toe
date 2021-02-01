87a66ebcb4ab92cf4cfd92a67b1d47891adaf30b
Jcom619 <joshuacompton@my.smccd.edu>
2021-01-31 19:31:50 -0800

    almost there?

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const squares = document.querySelectorAll('.box');
+    var squares = document.querySelectorAll('.box')
+    var playerDisplay = document.querySelector('#player')
+
+    let currentPlayer = 'playerX'
 
     squares.forEach(square => {
-        square.addEventListener('click', clickOutcome);
-    });
+        square.addEventListener('click', clickOutcome)
+    })
 
-    function clickOutcome() {
-        alert('clicked')
+    function clickOutcome(e) {
+        var squareArray = Array.from(squares)
+        var index = squareArray.indexOf(e.target)
+        playerDisplay.innerText = currentPlayer
+
+        if(currentPlayer === 'playerX') {
+            squares[index].classList.add('playerX')
+            curentPlayer = 'playerO'
+        } else {
+            squares[index].classlist.add('playerO')
+            currentPlayer = 'playerX'
+        }
     }
 })

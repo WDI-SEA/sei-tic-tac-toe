aca4902396c93dd5595e0c27a7a25dd71164fabe
ChrisB007 <chrisbitoy@gmail.com>
2021-02-01 11:36:15 -0500

    added playess

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Add eventListener to listen for click events
+
+// Add array of Players to the game, with "X" and "O" values.
+
+//Add an empty array to store the clicked "X and O" values.
+
+//Loop over the DOM elements, and randomly push the 'X and O' onClick.
+
+//Setup a reset button

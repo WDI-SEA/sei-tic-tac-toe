b9e783b4731c7d736d0cf1163ed03baca783a55f
ChrisB007 <chrisbitoy@gmail.com>
2021-02-02 10:30:32 -0500

    update to reset button

--- js/script.js
+++ js/script.js
@@ -2,11 +2,15 @@
 
 let player1 = document.querySelector("#playerone"),
     board = document.querySelector("#board"), currentPlayer, player2 = document.querySelector("#playertwo");
-let reset = document.querySelector("#resetmenot");
+let reset = document.querySelector("#reset");
 
 // Event Listener
 
-reset.addEventListener("click",()=> location.reload());
+reset.addEventListener("click",function(e){
+    console.log(e.target.value);
+    location.reload();
+}
+);
 
 
 
@@ -29,40 +33,3 @@ function selectPlayer() {
 }
 
 selectPlayer();
-
-
-
-
-
-
-//     // if (currentPlayer === player1){
-//     //     player1.addEventListener("click", function(e){
-//     //         console.log(e.target)
-//     //         player1.classList.add("buttoncolor")
-//     //     });
-//     //     document.querySelector("#selectplayer").textContent = `You have selected "X"`;
-//     //
-//     // } else if (currentPlayer === player2){
-//     //     player2.addEventListener("click", function(e){
-//     //         console.log(e.target.id="playertwo")
-//     //         player2.classList.add("buttoncolor")
-//     //     });
-//     //
-//     //     document.querySelector("#selectplayer").textContent = `You have selected "O"`;
-//     // }
-//
-//     return currentPlayer;
-// }
-//
-// selectPlayer(player2);
-
-
-// Add array of Players to the game, with "X" and "O" values.
-
-// Add eventListener to listen for click events.
-
-//Add an empty array to store the clicked "X and O" values.
-
-//Loop over the DOM elements, and randomly push the 'X and O' onClick.
-
-//Setup a reset button.

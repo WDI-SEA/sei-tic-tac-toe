8d840bb5ab3b57fa4d80d05810cc2248e69294fe
ornery-mouse <cory.sorel@gmail.com>
2021-10-15 17:57:40 -0500

    initial commit - html, css, and pseudoJS laid out

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//define the square
+//listen for click on square
+//identify which square was clicked
+//check which player's turn it is
+//check if square is available to click
+    //warn player
+    //reset turn
+//place X or O depending on turn
+//add square location to player's selected squares
+//compare to winning combinations
+//announce winner
+//or
+//toggle the turn
+//update turn display

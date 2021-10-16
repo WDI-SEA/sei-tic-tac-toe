ab504de96b3f1f0aafff1ed1678294b19e69ee0a
anaismveras <verasmanais@gmail.com>
2021-10-16 18:28:29 -0400

    adding logic to determine winner

--- js/script.js
+++ js/script.js
@@ -19,15 +19,14 @@ function playerOne() {
             if (clickedDiv.innerText == false) {
                 clickedDiv.innerText = 'X'
                 swithToPlayerTwo()
-            } else if (clickedDiv.innerText == 'X') {
-                    alert('THIS SQUARE IS ALREADY CLICKED')
-            } else {
-                clickedDiv.innerText = " "
-             }
-        })
-    }          
+            } else if (clickedDiv.innerText == 'O') {
+                clickedDiv.innerText = "X"
+                swithToPlayerTwo()
+            }
+        })     
+    }  
+    gamePlay()
 }
-
 
     function swithToPlayerTwo() {
         for (let i = 0; i < 9; i++) {
@@ -41,12 +40,62 @@ function playerOne() {
     }
 }
 
-// reset game
-function gameReset () { 
-    window.location.reload()
-}
 
-// press button to start game
-startButton.addEventListener('click', gameStart)
-// press button to reset game
-resetButton.addEventListener('click', gameReset)
+// const winngingConditions = [
+//     ['a1', 'a2', 'a3']
+//     ['b1', 'b2', 'b3']
+//     ['c1', 'c2', 'c3']
+//     ['a1', 'b1', 'c1']
+//     ['a2', 'b2', 'c2']
+//     ['a3', 'b3', 'c3']
+//     ['a1', 'b2', 'c3']
+//     ['c1', 'b2', 'a3']
+// ]
+
+const winngingConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+function gamePlay () {
+        for (let i = 0; i < winngingConditions.length; i++) {
+            div[i].addEventListener('click', (event) => {
+            const clickedDiv = event.target
+                for (let i = 0; i < 3; i++)
+                    if (winngingConditions[0] == clickedDiv.innerText) {
+                        console.log('winning')
+                } else {
+                        console.log('losing')
+                }
+            })
+        }
+}
+    
+    
+    // reset game
+    function gameReset () { 
+        window.location.reload()
+    }
+    
+    // press button to start game
+    startButton.addEventListener('click', gameStart)
+    // press button to reset game
+    resetButton.addEventListener('click', gameReset)
+    
+    
+    // const winning1 = [document.getElementById('square1'), document.getElementById('square2'), document.getElementById('square3')]
+    // const winning2 = [document.getElementById('square4'), document.getElementById('square5'), document.getElementById('square6')]
+    // const winning3 = [document.getElementById('square7'),document.getElementById('square8') , document.getElementById('square9')]
+    // const winning4 = [document.getElementById('square1'),document.getElementById('square4') , document.getElementById('square7')]
+    // const winning5 = [document.getElementById('square2'),document.getElementById('square5') , document.getElementById('square8')]
+    // const winning6 = [document.getElementById('square3'),document.getElementById('square6') , document.getElementById('square9')]
+    // const winning7 = [document.getElementById('square1'),document.getElementById('square5') , document.getElementById('square9')]
+    // const winning8 = [document.getElementById('square3'),document.getElementById('square5') , document.getElementById('square7')]
+    
+    // const winningConCondenced = [winning1, winning2, winning3, winning4, winning5,winning6,winning7,winning8]

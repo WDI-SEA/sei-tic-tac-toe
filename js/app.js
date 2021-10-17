7f5da1406701afa97deea2c85d1f8c806f24faa1
devinrbopp <devinrbopp@gmail.com>
2021-10-17 10:08:35 -0600

    basic game logic working incl wins

--- js/app.js
+++ js/app.js
@@ -15,10 +15,14 @@ const square9 = document.getElementById('div-9')
 
 // open turn counter
 turn = 0
+
 // establish win conditions
 const wins = [
+    // horizontals
     ['div-1','div-2','div-3'],['div-4','div-5','div-6'],['div-7','div-8','div-9'],
+    // verticals
     ['div-1','div-4','div-7'],['div-2','div-5','div-8'],['div-3','div-6','div-9'],
+    // diagonals
     ['div-1','div-5','div-9'],['div-3','div-5','div-7']
 ]
 
@@ -26,48 +30,58 @@ const wins = [
 const xMoves = []
 const oMoves = []
 
-// determine whose turn it is (iterator in a for loop using modulo to determine if the turn is even or odd)
-
+// function triggered by click on square
 const playerAction = (event) => {
-    const chosenSquareId = event.target.id
-    console.log('player chose:', chosenSquareId)
-    const newMark = document.createElement('p')
-    newMark.classList.add('mark')
-    //if else statement based on turn that assigns x or o
-    if (turn % 2 === 0) {
-        newMark.innerText = 'x'
-        xMoves.push(chosenSquareId)
+    // check whether the square already has been used
+    if (event.target.firstChild) {
+        console.log('invalid move')
     } else {
-        newMark.innerText = 'o'
-        oMoves.push(chosenSquareId)
-    }
-    document.getElementById(chosenSquareId).appendChild(newMark)
-    // check whether x has won
-    xMoves.sort()
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
-    for (i = 0; i < wins.length; i++) {
-        for(j = 0; j < wins[i].length; j++) {
-            for(k = 0; k < xMoves.length; k++) {
-                if (xMoves[k] === wins[i][j]) {
-
-                }
+        // if the square hasn't been used, id the square
+        const chosenSquareId = event.target.id
+        console.log('player chose:', chosenSquareId)
+        // create a mark to go in
+        const newMark = document.createElement('p')
+        newMark.classList.add('mark')
+        //if else statement based on turn that assigns x or o
+        if (turn % 2 === 0) {
+            newMark.innerText = 'x'
+            xMoves.push(chosenSquareId)
+        } else {
+            newMark.innerText = 'o'
+            oMoves.push(chosenSquareId)
+        }
+        document.getElementById(chosenSquareId).appendChild(newMark)
+        // function to check whether anyone has won
+        const isWinner = (winArray, playerArray) => {
+            return winArray.every( (element) => {
+                return playerArray.includes(element)
+            })
+        }
+        // check whether x won
+        for (i = 0; i < wins.length; i++) {
+            if(isWinner(wins[i], xMoves)) {
+                console.log('x wins')
+                break
             }
         }
-        
+        // check whether o won
+        for (i = 0; i < wins.length; i++) {
+            if(isWinner(wins[i], oMoves)) {
+                console.log('o wins')
+                break
+            }
+        }        
+        // check whether the board is full
+        console.log('x Moves:', xMoves)
+        console.log('o Moves:', oMoves)
+        if (turn === 8) {
+            console.log('game over')
+            // log arrays of current moves
+        }
+        // next turn
+        turn++
     }
-    
-    // check whether o has won
-    
-    
-    
-    // check whether the board is full
-    if (turn === 8) {
-        console.log('game over')
-    }
-    turn++
-    console.log('x Moves:', xMoves)
-    console.log('o Moves:', oMoves)
-}
+}   
 
 // listen for a click
 square1.addEventListener('click',playerAction)
@@ -79,24 +93,3 @@ square6.addEventListener('click',playerAction)
 square7.addEventListener('click',playerAction)
 square8.addEventListener('click',playerAction)
 square9.addEventListener('click',playerAction)
-// on click: 
-    //create a new mark element
-    // append new mark to the clicked square
-
-    // check whether x has reached win conditions
-
-    // if win conditions are met, announce that x has won and break loop
-
-       // create reset button
-
-        // append reset button
-
-// check whether the board is full
-
-    // if board is full, announce a tie and break loop
-
-    // display reset button
-
-    // append reset button
-
-// if neither condition is met, up turn counter

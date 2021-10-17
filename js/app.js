52541eca4644467897cd4ff312449845f0a69b66
Mackmiller <mackmiller19@gmail.com>
2021-10-17 19:35:23 -0400

    updates

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
-//declare nine variables for squares
+//DECLARE VARIABLES
+//game-related variables
+let oneWin = false //one = player one
+let twoWin = false //two = player two
+let count = 0 //click count
+let oneArray = [] //array of player one choices
+let twoArray = [] //array of player two choices
+let wins = [ //possible winning combinations
+    ["div1", "div2", "div3"],
+    ["div4", "div5", "div6"],
+    ["div7", "div8", "div9"],
+    ["div1", "div5", "div9"],
+    ["div1", "div4", "div7"],
+    ["div1", "div3", "div5"],
+    ["div2", "div5", "div8"],
+    ["div3", "div6", "div9"],
+    ["div7", "div5", "div3"],
+]
+
+//declare DOM manipulation variables
+let playerTurn = null //text that changes depending on whose turn
+let restartButton = null //restart button variable
+let gameBoard = null //board game container
+let boxText = null //variable that passes text for box from HTML
+
+//declare each box class as a variable (tried using IDs here at first, but kept getting errors.)
 const square1 = document.querySelector(".div1")
 const square2 = document.querySelector(".div2")
 const square3 = document.querySelector(".div3")
@@ -8,89 +33,129 @@ const square6 = document.querySelector(".div6")
 const square7 = document.querySelector(".div7")
 const square8 = document.querySelector(".div8")
 const square9 = document.querySelector(".div9")
-//declare text area for where X or O will appear
-const message1 = document.querySelector('#message1')
-const message2 = document.querySelector('#message2')
-const message3 = document.querySelector('#message3')
-const message4 = document.querySelector('#message4')
-const message5 = document.querySelector('#message5')
-const message6 = document.querySelector('#message6')
-const message7 = document.querySelector('#message7')
-const message8 = document.querySelector('#message8')
-const message9 = document.querySelector('#message9')
 
-//functions for creating text when each square is clicked
-const createText1 = () => {
-    //if square is clicked, x appears
-    message1.innerText = 'X'
-    console.log(message1) 
+playerTurn = document.querySelector(".game-stat") //text that changes depending on whose turn
+gameBoard = document.querySelector(".parent") //board game container
+restartButton = document.querySelector("#restart-game") //restart button variable
+
+//GAME FUNCTIONS
+
+//assigning X or O text to box
+const boxChosen = (event) => {
+    //if count is divisible by 2, player 2 is playing (player 2 exclusively plays on even number count)
+    if (count % 2 === 0) {
+        count++
+        event.target.innerText = "X"
+        event.target.style.backgroundColor = "#F3D9BE"
+        //box chosen propels the turn of the next player, player 2
+        playerTurn.innerHTML = "Player 2 turn"
+        //push results to player 1 array
+        oneArray.push(event.target.parentNode.className)
+    //else it is player 1 playing
+    }else{
+        count++
+        event.target.innerText = "O"
+        event.target.style.backgroundColor = "#E5F7DE"
+        //box chosen propels the turn of the next player, player 1
+        playerTurn.innerHTML = "Player 1 turn"
+        //push results to player 2 array
+        twoArray.push(event.target.parentNode.className);
+    }
+    console.log(count)
+    checkWin()
+} 
+
+//determine winner or tie
+const checkWin = () => {
+
+    //Looping through both arrays
+    for(let i=0; i <= wins.length - 1; i++){
+        let onePlayTurns = 0
+        let twoPlayTurns = 0
+        
+        for(let j=0; j <= wins[i].length - 1; j++){
+            //player 1 win conditional     
+            if(oneArray.includes(wins[i][j])){
+                onePlayTurns++
+            }
+                if(onePlayTurns === 3){
+                    playerTurn.innerHTML = "Player 1 won"
+                    oneWin = true
+                    console.log(onePlayTurns)
+                    console.log(oneWin)
+                    //console.log("Player 1 wins")
+                    setTimeout(() => { restartGame() }, 2000)   
+                } 
+            //player 2 win conditional 
+            if(twoArray.includes(wins[i][j])){
+                twoPlayTurns++
+                //console.log(twoPlayTurns)
+            } 
+                if(twoPlayTurns === 3){
+                    playerTurn.innerHTML = "Player 2 won"
+                    twoWin = true
+                    //console.log("player 2 wins")
+                    setTimeout(() => { restartGame() }, 2000)
+                } 
+            //tie conditional
+            if (count === 9 && onePlayTurns !== 3 && twoPlayTurns !== 3) {
+                playerTurn.innerHTML = "tie"
+                setTimeout(() => {restartGame() }, 2000)
+            }  
+        }
+    } 
 }
-const createText2 = () => {
-    //if square is clicked, x appears
-    message2.innerText = 'X'
-    console.log(message2) 
+
+//selecting a box
+const selectBox = (event) => {
+    boxText = event.target.innerHTML
+    //if inner text is blank, box is available for selection
+    if (boxText == "") {
+        boxChosen(event)
+    } else if (boxText !== "" && boxText !== restartButton){
+        alert("this box has already been claimed")
+    }
 }
-const createText3 = () => {
-    //if square is clicked, x appears
-    message3.innerText = 'X'
-    console.log(message3) 
-}
-const createText4 = () => {
-    //if square is clicked, x appears
-    message4.innerText = 'X'
-    console.log(message4) 
-}
-const createText5 = () => {
-    //if square is clicked, x appears
-    message5.innerText = 'X'
-    console.log(message5) 
-}
-const createText6 = () => {
-    //if square is clicked, x appears
-    message6.innerText = 'X'
-    console.log(message6) 
-}
-const createText7 = () => {
-    //if square is clicked, x appears
-    message7.innerText = 'X'
-    console.log(message7) 
-}
-const createText8 = () => {
-    //if square is clicked, x appears
-    message8.innerText = 'X'
-    console.log(message8) 
-}
-const createText9 = () => {
-    //if square is clicked, x appears
-    message9.innerText = 'X'
-    console.log(message9) 
+
+//restart game
+function restartGame() {
+    //reset html elements
+    playerTurn.innerHTML = "Click on a box to start the game"
+    square1.innerHTML = ""
+    square2.innerHTML = ""
+    square3.innerHTML = ""
+    square4.innerHTML = ""
+    square5.innerHTML = ""
+    square6.innerHTML = ""
+    square7.innerHTML = ""
+    square8.innerHTML = ""
+    square9.innerHTML = ""
+    square1.style.backgroundColor = "#EDEBE3"
+    square2.style.backgroundColor = "#EDEBE3"
+    square3.style.backgroundColor = "#EDEBE3"
+    square4.style.backgroundColor = "#EDEBE3"
+    square5.style.backgroundColor = "#EDEBE3"
+    square6.style.backgroundColor = "#EDEBE3"
+    square7.style.backgroundColor = "#EDEBE3"
+    square8.style.backgroundColor = "#EDEBE3"
+    square9.style.backgroundColor = "#EDEBE3"
+    //reset number of turns, click count, and arrays
+    onePlayTurns = 0
+    twoPlayTurns = 0
+    count = 0
+    oneArray = []
+    twoArray = []
+    oneWin = false //one = player one
+    twoWin = false //two = player two
+    console.log(oneWin)
+    console.log(twoWin)
 }
 
 
-//call items
+//EVENT LISTENERS
 document.addEventListener("DOMContentLoaded", () => {
-    //click square 1
-    square1.addEventListener("click", createText1)
-    //click square 2
-    square2.addEventListener("click", createText2)
-    //click square 3
-    square3.addEventListener("click", createText3)
-    //click square 4
-    square4.addEventListener("click", createText4)
-    //click square 5
-    square5.addEventListener("click", createText5)
-    //click square 6
-    square6.addEventListener("click", createText6)
-    //click square 7
-    square7.addEventListener("click", createText7)
-    //click square 8
-    square8.addEventListener("click", createText8)
-    //click square 9
-    square9.addEventListener("click", createText9)
-
-//NEXT:
-    //if statement for if certain square is selected
-
-        //if statement for other options once that square is selected
-    
+    //begin game
+    gameBoard.addEventListener("click", selectBox)
+    //restart game
+    restartButton.addEventListener("click", restartGame) 
 })

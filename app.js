8d869d9c3a2dd201cd64fbeaf28cc2389064d2cc
mgkdn9 <mkohlberg95@gmail.com>
2021-10-17 11:26:57 -0500

    Meets all requirements

--- app.js
+++ app.js
@@ -13,50 +13,106 @@ document.addEventListener('DOMContentLoaded', () => {
   const xWinsMessage = document.querySelector('.xWins')
   const oWinsMessage = document.querySelector('.oWins')
   let xTurn = true // Boolean for it being x's turn
-  let gameComplete = false
+  let gameWon = false
+  let catsGame = false
   const resetBtn = document.querySelector('button')
+  const xTurnIndicators = document.getElementsByClassName('xTurnIndicator')
+  const oTurnIndicators = document.getElementsByClassName('oTurnIndicator')
+  const catsGameIndicator = document.querySelector('.catsGameIndicator')
 
-  function checkGameComplete(){
+  function checkCatsGame(){
+    catsGame = true
+    for( let i = 0; i < tiles.length; i++){
+      if (!tiles[i].classList.contains('selected')){
+        catsGame = false
+      }
+    }
+    if(catsGame){
+      catsGameIndicator.style.display = 'initial'
+      xTurnIndicators[0].style.display = 'none'
+      xTurnIndicators[1].style.display = 'none'
+      oTurnIndicators[0].style.display = 'none'
+    }
+  }
+
+  function checkGameWon(){
     if (
-      topLeft.classList.contains('x')&&topCenter.classList.contains('x')&&topRight.classList.contains('x')
+      topLeft.classList.contains('x')&&topCenter.classList.contains('x')&&topRight.classList.contains('x') ||
+      centerLeft.classList.contains('x')&&centerCenter.classList.contains('x')&&centerRight.classList.contains('x') ||
+      bottomLeft.classList.contains('x')&&bottomCenter.classList.contains('x')&&bottomRight.classList.contains('x') ||
+      topLeft.classList.contains('x')&&centerLeft.classList.contains('x')&&bottomLeft.classList.contains('x') ||
+      topCenter.classList.contains('x')&&centerCenter.classList.contains('x')&&bottomCenter.classList.contains('x') ||
+      topRight.classList.contains('x')&&centerRight.classList.contains('x')&&bottomRight.classList.contains('x') ||
+      topLeft.classList.contains('x')&&centerCenter.classList.contains('x')&&bottomRight.classList.contains('x') ||
+      topRight.classList.contains('x')&&centerCenter.classList.contains('x')&&bottomLeft.classList.contains('x')
     ){
       xWinsMessage.style.display = 'initial'
-      gameComplete = true
+      gameWon = true
     }
     else if (
-      topLeft.classList.contains('o')&&topCenter.classList.contains('o')&&topRight.classList.contains('o')
+      topLeft.classList.contains('o')&&topCenter.classList.contains('o')&&topRight.classList.contains('o') ||
+      centerLeft.classList.contains('o')&&centerCenter.classList.contains('o')&&centerRight.classList.contains('o') ||
+      bottomLeft.classList.contains('o')&&bottomCenter.classList.contains('o')&&bottomRight.classList.contains('o') ||
+      topLeft.classList.contains('o')&&centerLeft.classList.contains('o')&&bottomLeft.classList.contains('o') ||
+      topCenter.classList.contains('o')&&centerCenter.classList.contains('o')&&bottomCenter.classList.contains('o') ||
+      topRight.classList.contains('o')&&centerRight.classList.contains('o')&&bottomRight.classList.contains('o') ||
+      topLeft.classList.contains('o')&&centerCenter.classList.contains('o')&&bottomRight.classList.contains('o') ||
+      topRight.classList.contains('o')&&centerCenter.classList.contains('o')&&bottomLeft.classList.contains('o')
     ){
       oWinsMessage.style.display = 'initial'
-      gameComplete = true
+      gameWon = true
+    }
+    if(gameWon){
+      xTurnIndicators[0].style.display = 'none'
+      xTurnIndicators[1].style.display = 'none'
+      oTurnIndicators[0].style.display = 'none'
+    } else {
+      checkCatsGame()
     }
   }
 
   function selectTile(tile){
-    if(!tile.classList.contains('selected') && !gameComplete)
+    if(!tile.classList.contains('selected') && !gameWon){
       if (xTurn){
         tile.classList.add('x')
         tile.innerText = 'x'
+        tile.style.color = 'red'
+        xTurnIndicators[0].classList.add('invisible')
+        xTurnIndicators[1].classList.add('invisible')
+        oTurnIndicators[0].classList.remove('invisible')
       } else {
         tile.classList.add('o')
         tile.innerText = 'o'
+        xTurnIndicators[0].classList.remove('invisible')
+        xTurnIndicators[1].classList.remove('invisible')
+        oTurnIndicators[0].classList.add('invisible')
       }
       // Signal that the tile has been selected
       tile.classList.add('selected')
       // Change turns
       xTurn = !xTurn
       // Check for game completion
-      checkGameComplete()
+      checkGameWon()
+    }
   }
 
   function reset(){
     for (let i = 0; i<tiles.length; i++){
       tiles[i].classList.remove('x','o','selected')
       tiles[i].innerText = ''
+      tiles[i].style.color = 'initial'
     }
     xTurn = true
-    gameComplete = false
+    gameWon = false
     xWinsMessage.style.display = 'none'
     oWinsMessage.style.display = 'none'
+    xTurnIndicators[0].style.display = 'initial'
+    xTurnIndicators[1].style.display = 'initial'
+    oTurnIndicators[0].style.display = 'initial'
+    xTurnIndicators[0].classList.remove('invisible')
+    xTurnIndicators[1].classList.remove('invisible')
+    oTurnIndicators[0].classList.add('invisible')
+    catsGameIndicator.style.display = 'none'
   }
 
     // add event listener to each tile to run selectTile()

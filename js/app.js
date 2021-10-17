159da6880c2ef44de78b1bb309dbab0a99821b3e
devinrbopp <devinrbopp@gmail.com>
2021-10-17 11:52:04 -0600

    finalized and added sound

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('the dom has loaded')
 })
 
+// create turn counter
+const turnCounter = document.getElementById('turn-counter')
+
 // open turn counter
 turn = 0
 
@@ -17,8 +20,8 @@ const wins = [
 ]
 
 // make array for x and o entries
-const xMoves = []
-const oMoves = []
+let xMoves = []
+let oMoves = []
 
 // make wins
 let xWins = false
@@ -29,6 +32,8 @@ const playerAction = (event) => {
     // check whether the square already has been used
     if (event.target.firstChild) {
         console.log('invalid move')
+    } else if (xWins || oWins) {
+        console.log('the game is over')
     } else {
         // if the square hasn't been used, id the square
         const chosenSquareId = event.target.id
@@ -70,28 +75,61 @@ const playerAction = (event) => {
                 break
             }
         }        
+        // next turn
+        turn++
+        console.log(turn)
+        if (turn % 2 === 0) {
+            const xTurnMessage = document.createElement('p')
+            xTurnMessage.innerText = 'It is x\'s turn'
+            turnCounter.innerHTML = ''
+            turnCounter.appendChild(xTurnMessage)
+        } else if (turn % 2 === 1) {
+            const oTurnMessage = document.createElement('p')
+            oTurnMessage.innerText = 'It is o\'s turn'
+            turnCounter.innerHTML = ''
+            turnCounter.appendChild(oTurnMessage)
+        }
         // check whether the board is full
         if (xWins) {
             const xAnnounce = document.createElement('p')
             xAnnounce.innerText = 'X wins!'
-            resultField.innerHTML = ''
-            resultField.appendChild(xAnnounce)
+            turnCounter.innerHTML = ''
+            turnCounter.appendChild(xAnnounce)
         } else if (oWins) {
             const oAnnounce = document.createElement('p')
             oAnnounce.innerText = 'O wins!'
-            resultField.innerHTML = ''
-            resultField.appendChild(oAnnounce)
+            turnCounter.innerHTML = ''
+            turnCounter.appendChild(oAnnounce)
+        } else if (turn === 9) {
+            const tieAnnounce = document.createElement('p')
+            tieAnnounce.innerText = 'Cat\'s game! Try again.'
+            turnCounter.innerHTML = ''
+            turnCounter.appendChild(tieAnnounce)
+            const horns = new Audio('sounds/the-price-is-right-losing-horn.mp3')
+            horns.play()
         }
-        console.log('x Moves:', xMoves)
-        console.log('o Moves:', oMoves)
-        if (turn === 8) {
-            console.log('game over')
-            // log arrays of current moves
-        }
-        // next turn
-        turn++
     }
 }   
+
+// reset function
+const reset = () => {
+    turn = 0
+    xMoves = []
+    oMoves = []
+    xWins = false
+    oWins = false
+    square1.innerHTML = ''
+    square2.innerHTML = ''
+    square3.innerHTML = ''
+    square4.innerHTML = ''
+    square5.innerHTML = ''
+    square6.innerHTML = ''
+    square7.innerHTML = ''
+    square8.innerHTML = ''
+    square9.innerHTML = ''
+    turnCounter.innerHTML = '<p>X goes first. Select a square.</p>'
+}
+
 
 // create clickable squares 1-9
 const square1 = document.getElementById('div-1')
@@ -104,7 +142,7 @@ const square7 = document.getElementById('div-7')
 const square8 = document.getElementById('div-8')
 const square9 = document.getElementById('div-9')
 
-const resultField = document.getElementById('results')
+const resetButton = document.getElementById('reset')
 
 // listen for a click
 square1.addEventListener('click',playerAction)
@@ -116,3 +154,5 @@ square6.addEventListener('click',playerAction)
 square7.addEventListener('click',playerAction)
 square8.addEventListener('click',playerAction)
 square9.addEventListener('click',playerAction)
+
+resetButton.addEventListener('click',reset)

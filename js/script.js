9d8e7b4f226c14ee808bdafbefbf0f6731466184
shuzel99 <shuzellide@gmail.com>
2021-10-19 19:26:38 -0400

    adding event listeners idk

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
-function myfunc(){
-    var [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    1 = document.getElementById("1").value;
-    2 = document.getElementById("2").value;
-    3 = document.getElementById("3").value;
-    4 = document.getElementById("4").value;
-    5 = document.getElementById("5").value;
-    6 = document.getElementById("6").value;
-    7 = document.getElementById("7").value;
-    8 = document.getElementById("8").value;
-    9 = document.getElementById("9").value;
 
-    if ((1 == 'X' || 1 == 'x') && (2 == 'x' || 2 == 'X') && (3 == 'x' || 3 == 'X')) {
-        document.getElementById('print')
-            .innerHTML = "Player X won"
-        document.getElementById("4").disabled = true;
-        document.getElementById("5").disabled = true;
-        document.getElementById("6").disabled = true;
-        document.getElementById("7").disabled = true;
-        document.getElementById("8").disabled = true;
-        document.getElementById("9").disabled = true;
-    window.alert('Player X won');
-    }
+// check for winner, check for tie, cant replay a box//
+
+const boxOrder = () => {
+    var 1, 2, 3, 4, 5, 6, 7, 8, 9;
+    const square1 = document.getElementById("#1").value;
+    const square2 = document.getElementById("#4").value;
+    const square3 = document.getElementById("#7").value;
+    const square4 = document.getElementById("#2").value;
+    const square5 = document.getElementById("#5").value;
+    const square6 = document.getElementById("#8").value;
+    const square7 = document.getElementById("#3").value;
+    const square8 = document.getElementById("#6").value;
+    const square9 = document.getElementById("#9").value;
+}
+    
+const clickExOh = () => {
+    square1.addEventListener('click', () => {
+         square1.innerHTML = "X";
+        }
+    square2.addEventListener('click', () => { add if else here }
+    square3.addEventListener('click', () => { add if else here }
+    square4.addEventListener('click', () => { add if else here }
+    square5.addEventListener('click', () => { add if else here }
+    square6.addEventListener('click', () => { add if else here }
+    square7.addEventListener('click', () => { add if else here }
+    square8.addEventListener('click', () => { add if else here }
+    square9.addEventListener('click', () => { add if else here }
+}
 
 }

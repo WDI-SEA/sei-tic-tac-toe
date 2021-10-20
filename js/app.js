20566929ccc6887563424e5bd171cc4e478f4808
hannahlegros <hannahrlegros@gmail.com>
2021-10-19 23:11:47 -0400

    remodel

--- js/app.js
+++ js/app.js
@@ -7,41 +7,99 @@ let tiedGame = []
 
 const playGame = () => {
     if (xTurn === true){
+        turnMessage.innerText = "Time for player O to make a choice"
         xTurn = false
-        // console.log(xTurn)
         playerMove()
     }
     else {
+        turnMessage.innerText = "Time for player X to make a choice"
         xTurn = true
         playerMove()
     }
+    // console.log(xTurn)
 }
+square.forEach((box) => {
+    box.addEventListener("click", playGame)
+})
+console.log(square.forEach())
+
+//problem: accessing the individual .innerText of .square (class of multiple buttons)
+        //solving: getting the X and O to appear on game board
+        //solving: make squares unclickable after clicked/after game over
+//problem: adding elements to gameTied array 
+        //solving: determining the tied game
+    //also, make sure reset button works once x/o input works
+
+// document.getElementById("button-1").addEventListener("click", playGame)
+// document.getElementById("button-2").addEventListener("click", playGame)
+// document.getElementById("button-3").addEventListener("click", playGame)
+// document.getElementById("button-4").addEventListener("click", playGame)
+// document.getElementById("button-5").addEventListener("click", playGame)
+// document.getElementById("button-6").addEventListener("click", playGame)
+// document.getElementById("button-7").addEventListener("click", playGame)
+// document.getElementById("button-8").addEventListener("click", playGame)
+// document.getElementById("button-9").addEventListener("click", playGame)
+
 const playerMove = () => {
     if (!square.innerText && xTurn === true){
         square.innerText = "X"
+        checkForWinner()
         tiedGame.push("X")
-        // checkForWinner()
     }
     else if (!square.innerText && xTurn === false){
         square.innerText = "O"
+        checkForWinner()
         tiedGame.push("O")
-        // checkForWinner()
-        console.log(tiedGame)
+        // console.log(square.innerText)
     }
 }
+const checkForWinner = () => {
+    winningComb = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    for(let i = 0; i < winningComb.length; i++) {
+        const players = ["X", "O"]
+        let firstOne = square[winningComb[i][0]].innerText
+        if (firstOne === square[winningComb[i][1]].innerText &&
+            firstOne === square[winningComb[i][2]].innerText &&
+            players.includes(firstOne)) {
+            if (xTurn) {
+                gameEndMessage.innerText = "Player X Wins!"
+            }
+            else if (!xTurn) {
+                gameEndMessage.innerText = "Player O Wins!"
+            }
+            square.disabled = true
+        }
+        else if (tiedGame.length >= 8){
+                gameEndMessage.innerText = "Cat's game!"
+                square.disabled = true
+            }                      
+        else {
+                return
+            }
+        }
+    }
+
+
+const resetGame = () => {
+    square.innerText = ""
+    gameEndMessage.innerText = "New game! Pick a square."
+    turnMessage.innerText= ""
+    square.disabled = false
+}
+
+reset.addEventListener("click", resetGame)
 
 
 
-
-document.getElementById("button-1").addEventListener("click", playGame)
-document.getElementById("button-2").addEventListener("click", playGame)
-document.getElementById("button-3").addEventListener("click", playGame)
-document.getElementById("button-4").addEventListener("click", playGame)
-document.getElementById("button-5").addEventListener("click", playGame)
-document.getElementById("button-6").addEventListener("click", playGame)
-document.getElementById("button-7").addEventListener("click", playGame)
-document.getElementById("button-8").addEventListener("click", playGame)
-document.getElementById("button-9").addEventListener("click", playGame)
 
 
 
@@ -71,38 +129,6 @@ document.getElementById("button-9").addEventListener("click", playGame)
 // const tiedGame = []
 
 
-// const checkForWinner = () => {
-//     winningComb = [
-//         [0,1,2],
-//         [3,4,5],
-//         [6,7,8],
-//         [0,3,6],
-//         [1,4,7],
-//         [2,5,8],
-//         [0,4,8],
-//         [2,4,6]
-//     ]
-//     for(let i = 0; i < winningComb.length; i++) {
-//         const players = ["X", "O"]
-//         let firstOne = move[winningComb[i][0]].innerText
-//         if (firstOne === move[winningComb[i][1]].innerText &&
-//             firstOne === move[winningComb[i][2]].innerText &&
-//             players.includes(firstOne)) {
-//             if (playerXTurn) {
-//                 message.innerText = "Player O Wins!"
-//             }
-//             else if (!playerXTurn) {
-//                 message.innerText = "Player X Wins!"
-//             }
-//             else if (tiedGame.length >= 8){
-//                 message.innerText = "Cat's game!"
-//             }                      
-//              else {
-//             }
-//         }
-//     }
-//     // playGame()
-// }
 
 // const playerMoveX = () => {
 //     if (playerXTurn === true && !move.innerText) {
@@ -132,11 +158,7 @@ document.getElementById("button-9").addEventListener("click", playGame)
 // const test = () => {
 //     console.log("test")
 // }
-// const resetGame = () => {
-//     window.location.reload()
-// }
 
-// reset.addEventListener("click", resetGame)
 
 // document.getElementById("button-1").addEventListener("click", playGame)
 // document.getElementById("button-2").addEventListener("click", playGame)

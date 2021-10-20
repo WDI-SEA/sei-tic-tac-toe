728469e6786853c5d6a76be8e3efa3fe66648fde
shuzel99 <shuzellide@gmail.com>
2021-10-20 00:05:05 -0400

    deleted buttons. added single click only to boxes

--- js/script.js
+++ js/script.js
@@ -1,31 +1,19 @@
 
 // check for winner, check for tie, cant replay a box//
 
-const boxOrder = () => {
-    var 1, 2, 3, 4, 5, 6, 7, 8, 9;
-    const square1 = document.getElementById("#1").value;
-    const square2 = document.getElementById("#4").value;
-    const square3 = document.getElementById("#7").value;
-    const square4 = document.getElementById("#2").value;
-    const square5 = document.getElementById("#5").value;
-    const square6 = document.getElementById("#8").value;
-    const square7 = document.getElementById("#3").value;
-    const square8 = document.getElementById("#6").value;
-    const square9 = document.getElementById("#9").value;
-}
-    
-const clickExOh = () => {
-    square1.addEventListener('click', () => {
-         square1.innerHTML = "X";
-        }
-    square2.addEventListener('click', () => { add if else here }
-    square3.addEventListener('click', () => { add if else here }
-    square4.addEventListener('click', () => { add if else here }
-    square5.addEventListener('click', () => { add if else here }
-    square6.addEventListener('click', () => { add if else here }
-    square7.addEventListener('click', () => { add if else here }
-    square8.addEventListener('click', () => { add if else here }
-    square9.addEventListener('click', () => { add if else here }
-}
+// const clickSquare = () => {
+//     const xOrO = () => {
+//         const xOrO = document.createElement("X" || "O")
+//         document.querySelector('.button').appendChild(xOrO)
+//     }
+// }
+// document.addEventListener('click', ()=> {
 
-}
+// })
+const cellElements = document.querySelectorAll("div.cell")
+    cellElements.forEach(cell => {
+        cell.addEventListener("click", handleClick, {once: true})
+    })
+    function handleClick(e) {
+        console.log('clicked')
+    }

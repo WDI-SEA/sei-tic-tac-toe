6a28209593a3dbc96f864a425d1d8caac1286385
hannahlegros <hannahrlegros@gmail.com>
2021-10-20 20:13:48 -0400

    Cleaned up done

--- js/app.js
+++ js/app.js
@@ -27,21 +27,8 @@ const playGame = (e) => {
         xTurn = false
         playerMove(clickedSquare)
     }
-}
-
-
-//problem: accessing the individual .innerText of .square (class of multiple buttons)
-        //solving: getting the X and O to appear on game board
-        //solving: make squares unclickable after clicked/after game over
-//problem: adding elements to gameTied array 
-        //solving: determining the tied game
-    //also, make sure reset button works once x/o input works
-
-    
+}   
 const playerMove = (clickedSquare) => {
-        // console.log("what's this?\n", someSquare)
-        // someSquare.innerText = "I clicked this"
-        // console.log("My logic good?", square.innerText)
     if (!clickedSquare.innerText && xTurn === true){
         clickedSquare.innerText = "X"
         tiedGame.push("X")
@@ -64,12 +51,8 @@ const checkForWinner = () => {
         [sq1.innerText,sq5.innerText,sq9.innerText],
         [sq3.innerText,sq5.innerText,sq7.innerText]
     ]
-    // const playerWins = [
-    //     ['X', 'X', 'X'],
-    //     ['O', 'O', 'O']
-    // ]
     for(let i = 0; i < winningComb.length; i++) {
-        console.log(winningComb[i])
+        // console.log(winningComb[i])
         if (winningComb[i][0] === 'X' &&
             winningComb[i][1] === 'X' &&
             winningComb[i][2] === 'X') {
@@ -102,7 +85,7 @@ const checkForWinner = () => {
         }
     }
     if (tiedGame.length >= 9){
-        console.log("TIED GAME\n", tiedGame)
+        // console.log("TIED GAME\n", tiedGame)
         gameEndMessage.innerText = "Cat's game!"
         turnMessage.innerText = ""
     }                      
@@ -110,9 +93,6 @@ const checkForWinner = () => {
         return
     }
 }
-
-
-
 const resetGame = () => {
     sq1.innerText = ""
     sq2.innerText = ""
@@ -147,86 +127,3 @@ document.getElementById("button-6").addEventListener("click", playGame)
 document.getElementById("button-7").addEventListener("click", playGame)
 document.getElementById("button-8").addEventListener("click", playGame)
 document.getElementById("button-9").addEventListener("click", playGame)
-
-//     [0,1,2],
-//     [3,4,5],
-//     [6,7,8],
-//     [0,3,6],
-//     [1,4,7],
-//     [2,5,8],
-//     [0,4,8],
-//     [2,4,6]
-// ]
-//     const players = ["X", "O"]
-//     let firstOne = square[winningComb[i][0]].innerText
-//     if (firstOne === square[winningComb[i][1]].innerText &&
-//         firstOne === square[winningComb[i][2]].innerText &&
-//         players.includes(firstOne)) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const move = document.querySelectorAll(".square")
-// const turn = document.querySelector("#turn")
-// const message = document.querySelector("#message")
-// let playerXTurn = true
-// const reset = document.querySelector("#reset")
-// const tiedGame = []
-
-
-
-// const playerMoveX = () => {
-//     if (playerXTurn === true && !move.innerText) {
-//         move.innerText = "X"
-//         tiedGame.push("X")
-//         playerXTurn = false
-//         turn.innerText = "Time for Player O to make a choice."
-//         checkForWinner()
-//     } 
-// }
-// const playerMoveO = () => {
-//     if (playerXTurn === false && !move.innerText) {
-//         move.innerText = "O"
-//         tiedGame.push("O")
-//         playerXTurn = true
-//         turn.innerText = "Time for Player X to make a choice"
-//         checkForWinner()
-//     } 
-// }
-// const playGame = () => {
-//     if (playerXTurn) {
-//         playerMoveX()
-//     } else {
-//         playerMoveO()
-//     }
-// }
-// const test = () => {
-//     console.log("test")
-// }
-
-
-
-
-//click on any square on the board (event listener)
-//fill in square with x, then o (repeat or give error message)
-//check for winner (loop)
-//if winner or tie, give message
-//stop clickability when game ends
-//reset button clears game and restores clickability

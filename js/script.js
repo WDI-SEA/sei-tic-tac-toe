26a0d2dbca6c4715aaa9b56daa650480d9e3bae0
Lawrencesalinas <lawrencesalinas@yahoo.com>
2021-10-21 23:51:57 -0400

    tic tac toe  functional version

--- js/script.js
+++ js/script.js
@@ -1,48 +1,66 @@
-const resetDiv = document.querySelector(".reset")
+const resetGane = document.getElementById("restart")
 const boxes = document.querySelectorAll(".box")
 const ticTac = document.getElementById("tic-tac-board")
 let xturn = true
 
 
- 
+const startReset = (e) => {
+  const reset = e.target.resetGane
+  boxes.forEach(box => {
+    box.addEventListener("click", clickBox, {once:true})
+    })
+    boxes.forEach(box =>{
+      box.classList.remove('x')
+      box.classList.remove("o")
+      resulta.classList.remove("xW")
+      resulta.classList.remove("oW")
+      resulta.classList.remove("d")
+    })
+    playerTurn.innerText = "player X turn"
+}
+
+
 const clickBox =(e) => {
+
     const boxName = e.target.classList[1]
      if(xturn){
-        e.target.classList.add('x')
+        e.target.classList.add("x")
         xturn = !xturn
-       checkWinner()          
-       turnToggle()               
+        turnToggle()  
+        checkWinner()     
+        checkDraw()     
+                
      } else {
-        e.target.classList.add('o')
+        e.target.classList.add("o")
         xturn = !xturn
-       checkWinner()
-    turnToggle()
+        turnToggle()
+        checkWinner()
+        checkDraw()
+      
     }
   }
 
-  
- 
-const checkWinner = () => {
-    let box0 = cell0.classList.contains("x")
-    let box1 = cell1.classList.contains("x")
-    let box2 = cell2.classList.contains("x")
-    let box3 = cell3.classList.contains("x")
-    let box4 = cell4.classList.contains("x")
-    let box5 = cell5.classList.contains("x")
-    let box6 = cell6.classList.contains("x")
-    let box7 = cell7.classList.contains("x")
-    let box8 = cell8.classList.contains("x")
 
-    let boxa = cell0.classList.contains("o")
-    let boxb = cell1.classList.contains("o")
-    let boxc = cell2.classList.contains("o")
-    let boxd = cell3.classList.contains("o")
-    let boxe = cell4.classList.contains("o")
-    let boxf = cell5.classList.contains("o")
-    let boxg = cell6.classList.contains("o")
-    let boxh = cell7.classList.contains("o")
-    let boxi = cell8.classList.contains("o")
-   
+const checkWinner = () => {
+let box0 = cell0.classList.contains("x")
+let box1 = cell1.classList.contains("x")
+let box2 = cell2.classList.contains("x")
+let box3 = cell3.classList.contains("x")
+let box4 = cell4.classList.contains("x")
+let box5 = cell5.classList.contains("x")
+let box6 = cell6.classList.contains("x")
+let box7 = cell7.classList.contains("x")
+let box8 = cell8.classList.contains("x")
+let boxa = cell0.classList.contains("o")
+let boxb = cell1.classList.contains("o")
+let boxc = cell2.classList.contains("o")
+let boxd = cell3.classList.contains("o")
+let boxe = cell4.classList.contains("o")
+let boxf = cell5.classList.contains("o")
+let boxg = cell6.classList.contains("o")
+let boxh = cell7.classList.contains("o")
+let boxi = cell8.classList.contains("o")
+
      if((box0 && box1 && box2 === true) || 
         (box2 && box5 && box8 === true) ||
         (box0 && box3 && box6 === true) ||
@@ -53,7 +71,7 @@ const checkWinner = () => {
         (box3 && box4 && box5 === true))
      {
        
-            resulta.classList.add('xW')
+            resulta.classList.add("xW")
             boxes.forEach(box => {
               box.removeEventListener("click", clickBox)
             })
@@ -68,55 +86,56 @@ const checkWinner = () => {
         (boxa && boxe && boxi === true) ||
         (boxc && boxe && boxg === true))
         {
-            resulta.classList.add('oW')
+            resulta.classList.add("oW")
             boxes.forEach(box => {
               box.removeEventListener("click", clickBox)
                 })
           
-        }  
-    }
+        } 
+      }
+
     
 const turnToggle = () => {
     if(xturn){
-    player.innerText = "X turn"
+      playerTurn.innerText = "player X turn"
     } else {
-        player.innerText = "O turn"
+        playerTurn.innerText = "player O turn"
+    }
+   }
+    
+
+const checkDraw = () => {  
+  let box0 = cell0.classList.contains("x")
+  let box1 = cell1.classList.contains("x")
+  let box2 = cell2.classList.contains("x")
+  let box3 = cell3.classList.contains("x")
+  let box4 = cell4.classList.contains("x")
+  let box5 = cell5.classList.contains("x")
+  let box6 = cell6.classList.contains("x")
+  let box7 = cell7.classList.contains("x")
+  let box8 = cell8.classList.contains("x")
+  let boxa = cell0.classList.contains("o")
+  let boxb = cell1.classList.contains("o")
+  let boxc = cell2.classList.contains("o")
+  let boxd = cell3.classList.contains("o")
+  let boxe = cell4.classList.contains("o")
+  let boxf = cell5.classList.contains("o")
+  let boxg = cell6.classList.contains("o")
+  let boxh = cell7.classList.contains("o")
+  let boxi = cell8.classList.contains("o")
+ 
+   if((box0 || boxa) && (box1 || boxb) && (box2 || boxc) &&
+      (box3 || boxd) && (box4 || boxe) && (box5 || boxf) &&
+      (box6 ||boxg) && (box7 || boxh ) && (box8 || boxi)) {
+        console.log('hi')
+        resulta.classList.add("d")
+      }
     }
 
-}
-    
-   const checkDraw = () => {
-       return [...boxes].every
-   }
-   
 
-  
-
-    // if((box0                                                                                      
-    // && box1 && box2 === x || o) || (box2 && box5 && box8)){
-    //     console.log('we have a winner')
-    // }
-
-    
-    
-
-
-
-
-
-//event handlers
-const handleReset = (e) => {
-    console.log(e)
-}
-        
-
-    
-   
-resetDiv.addEventListener('click', handleReset);
-
-
-//event listeners
+restart.addEventListener("click", startReset)
 boxes.forEach(box => {
     box.addEventListener("click", clickBox, {once:true})
     })
-// resetDiv.addEventlistener("click", handleReset)
+
+    

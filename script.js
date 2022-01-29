96f88a7241eb9f838760c05121f8e94655107e70
brnguy <nguyen.bryan15@gmail.com>
2022-01-28 16:57:30 -0800

    Added files and planning

---
+++ script.js
@@ -0,0 +1,24 @@
+// What kind of variables will I need?
+    // data container (array) to hold who has gone where (the gameboard)
+    // (optional) --- define each players choice
+    // Whose turn it is
+    // Winning Combinations to Check (data container?)
+    // Toggle/Boolean if someone has won
+    // Cats Option 1: Board is Full
+    // Cats Option 2: Turn Count for Cats Game
+
+// What will happen when the user clicks?
+    // Selection of X or O (who's turn it is?)
+    // Mark the space if it is available
+        // Do nothing or add message if it is marked
+    // Check if there is a winner
+        // If there is a winner -- stop gameplay and show who won
+        // if not -- let gameplay continue
+
+// Stretch Goals
+    // Cool CSS
+    // Timers for Turns
+    // Let User Select Side
+    // AI
+        // Level 1 is Random AI
+        // Level 2 is set of moves

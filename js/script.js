8b934a7384c750e6d4e1f9261ef1adadb45d633d
kokopuffz <chanheeya@gmail.com>
2022-02-02 02:40:55 -0600

    stop buttons work, add img issues

--- js/script.js
+++ js/script.js
@@ -5,7 +5,15 @@ let oMoves =[]
 let winningMoves = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,7]]
 let buttons = document.getElementsByTagName('button');
 
+let gameOver = false;
 // checks oMoves and xMoves to see if it matches winningMoves
+
+// function add_img() {
+//   let img = document.createElement('img');
+//   img.src = '/img/poe.png'
+//   document.getElementById("body").appendChild(img)
+// }
+
 let winCheck = (moves) => {
   for (let i = 0; i < winningMoves.length; i++) {
     let winningNums = winningMoves[i]
@@ -16,43 +24,28 @@ let winCheck = (moves) => {
   return false
 }
 
-// let usedMoves = (num) => {
-//   const index = oMovesChoices.indexOf(num);
-//   if (index > -1) {
-//     oMovesChoices.splice(index,1);
-//   }
-// }
-
-// let getOMoves = () => {
-//   const index = Math.floor(Math.random() * oMovesChoices.length)
-//     return oMovesChoices[index]
-// }
-
-// let gameOver = () => {
-//   if (winCheck(oMoves) = false || winCheck(xMoves = false)) && (oMoves.length + xMoves.length == 9) {
-
-//   }
-// }
-
 for (let button of buttons) {
-  button.addEventListener("click", function(event) {
-    const pick = (parseInt(event.target.id))
+  button.addEventListener("click", function(input) {
+    const pick = (parseInt(input.target.id))
+
+    if (gameOver){
+      return;
+    }
+
     if (xMoves.includes(pick) || oMoves.includes(pick)) {
       // document.getElementById("poewon").src="/img/poe.png"
       document.getElementById("notes").innerText = (`Already Taken`)
       return 
     }
-  
-    
-remove event listener...
     
     if (xMoves.length > oMoves.length) {
-      event.target.innerText = "o";
+      input.target.innerText = "o";
       oMoves.push(pick)
-      document.getElementById("notes").innerText = (`o move!`)
-    } else if (xMoves.length === oMoves.length) {
       document.getElementById("notes").innerText = (`x move!`)
-      event.target.innerText = "x";
+
+    } else if (xMoves.length === oMoves.length) {
+      document.getElementById("notes").innerText = (`o move!`)
+      input.target.innerText = "x";
       xMoves.push(pick)
     }
 
@@ -60,27 +53,32 @@ remove event listener...
     console.log(`ommoves: ${oMoves}`)
     console.log(pick)
 
-    // console.log(`omovechoices: ${oMovesChoices}`)
-
-
-
-    if (winCheck(xMoves)) {
+    if (winCheck(xMoves) === true) {
+      gameOver = true;
+      // button.replaceWith(button.cloneNode(true));
       document.getElementById("notes").innerText = ("x Won!")
-      // document.getElementById("poewon").src="/img/poe.png"
+      return
+        
     }
 //image element add src 
 
     if (winCheck(oMoves)) {
+      gameOver = true;
+      // let poe =document.createElement("img");
+      // poe.setAttribute("src", "/img/poe.png")
+      // document.getElementById("poetie").appendChild("poe");
       document.getElementById("notes").innerText = ("o Won!")
-        return
+      return
     }
 
     if ((oMoves.length + xMoves.length == 9) && (winCheck(xMoves)=== false) && (winCheck(oMoves) === false)) {
+      gameOver = true;
       document.getElementById("notes").innerText = ("It's a Tie!")
-        return
+      return
     }
 
   })
     
 
 }
+

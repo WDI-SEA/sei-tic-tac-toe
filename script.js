69490ae1e03fc946cc55095c214e94e8a01d4c94
Triston J Palacios <97129216+tristonpalacios@users.noreply.github.com>
2022-02-01 20:00:17 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -17,21 +17,36 @@ let toeScore = 0;
 let tieStatus =0;
 let winner = X;
 
-  
 
 
-const pieceClicked = (tile) => {
-    const place = tile.target.id;
+
+const pieceClicked = (tile) => {const place = tile.target.id;
+    board[place]== currentPlayer;
+    if(!board[place]){
+        board[place]=previousPlayer;tile.target;
+        console.log(Array)
+
+    }
     if(board[place] === board[place]){
         tile.target.innerText = currentPlayer;
     board[place]=currentPlayer;
+    checkBoard()
    if(turnCounter === 0){
        currentPlayer=X, previousPlayer=O}else if (turnCounter ===1) {
-           currentPlayer=O,previousPlayer=X} else if (turnCounter ===2 ) {currentPlayer=X,previousPlayer=O}else if (turnCounter===3) {currentPlayer=O, previousPlayer=X}else if (turnCounter ===4) {currentPlayer=X, previousPlayer=O}else if (turnCounter===5) {currentPlayer=O, previousPlayer=X}else if (turnCounter ===6) {currentPlayer=X, previousPlayer=O}else if (turnCounter===7) {currentPlayer=O, previousPlayer=X}
-           else if (turnCounter ===8) {currentPlayer=X, previousPlayer=O}
+           currentPlayer=O,previousPlayer=X} else if (turnCounter ===2 ) {currentPlayer=X,previousPlayer=O}else if (turnCounter===3) {currentPlayer=O, previousPlayer=X}else if (turnCounter ===4) {currentPlayer=X, previousPlayer=O}else if (turnCounter===5) {currentPlayer=O, previousPlayer=X}else if (turnCounter ===6) {currentPlayer=X, previousPlayer=O}else if (turnCounter===7) {currentPlayer=O, previousPlayer=X}else if (turnCounter ===8) {currentPlayer=X, previousPlayer=O}else if (turnCounter ===9) {currentPlayer=O, previousPlayer=X,checkBoard(),winMes.innerText=`It's a tie`}else if (turnCounter ===10) {tieCondition()}
+
 
            
-            checkBoard()
+           
+
+
+           checkBoard()
+            
+            
+           
+            
+           
+            
     
            turnCounter++;
            console.log(turnCounter)
@@ -41,12 +56,28 @@ const pieceClicked = (tile) => {
            
        }
 
+       
+
        checkBoard = () => {if(previousPlayer===board[0]&&
         board[0] == board[1] 
        && board[0] == board[2])
        {console.log(`winner`), winMes.innerText=`${previousPlayer} wins!` , endGame(),winner = previousPlayer}
        else if(previousPlayer===board[3]&&board[3] == board[4] && board[3] == board[5]){ winMes.innerText=`${previousPlayer} wins!` , endGame(),tieStatus=0}else if(previousPlayer===board[6]&&board[6] == board[7] && board[6] == board[8]){ winMes.innerText=`${previousPlayer} wins!` , endGame(),tieStatus=0}else if(previousPlayer===board[0]&&board[0] == board[3] && board[0] == board[6]){ winMes.innerText=`${previousPlayer} wins!` , endGame(),tieStatus=0}else if(previousPlayer===board[1]&&board[1] == board[4] && board[1] == board[7]){ winMes.innerText=`${previousPlayer} wins!` , endGame(),tieStatus=0}else if(previousPlayer===board[2]&&board[2] == board[5] && board[2] == board[8]){ winMes.innerText=`${previousPlayer} wins!` , endGame(),tieStatus=0}else if(previousPlayer===board[0]&&board[0] == board[4] && board[0] == board[8]){ winMes.innerText=`${previousPlayer} wins!` , endGame(),tieStatus=0}else if(previousPlayer===board[2]&&board[2] == board[4] && board[2] == board[6]){ winMes.innerText=`${previousPlayer} wins!` , endGame(),tieStatus=0}
-       else if(turnCounter>=9){console.log(`tie`), winMes.innerText=`It's a tie` ,tieStatus++, endGame(),tieCorrector()}
+       
+
+
+
+       
+
+       tieCondition = ()=>{
+        checkBoard();
+        // if(turnCounter===9){console.log(`tie`), winMes.innerText=`It's a tie` , endGame(),tieCorrector()}}
+        // if(previousPlayer!=board[0]&&board[0] == board[1] 
+        //    && board[0] == board[2]&&previousPlayer!=board[3]&&board[3] == board[4] && board[3] == board[5]&&previousPlayer!=board[6]&&board[6] == board[7] && board[6] == board[8]&&previousPlayer!=board[0]&&board[0] == board[3] && board[0] == board[6]&&previousPlayer!=board[1]&&board[1] == board[4] && board[1] == board[7]&&previousPlayer!=board[2]&&board[2] == board[5] && board[2] == board[8]&&previousPlayer!=board[0]&&board[0] == board[4] && board[0] == board[8]&&previousPlayer!=board[2]&&board[2] == board[4] && board[2] == board[6]){turnCounter++}else 
+        if(turnCounter===10){console.log(`tie`), winMes.innerText=`It's a tie` , endGame(),tieCorrector()}}
+
+       
+        
       
        endGame = () =>{
            console.log(`Game is over`),
@@ -62,17 +93,12 @@ const pieceClicked = (tile) => {
            console.log(`the winner is ${winner}`)
        }
 
-       tieCorrector=()=>{
-           if(turnCounter>=9){
-               toeScore=toeScore-1
-           }
-       }
+    //    tieCorrector=()=>{
+    //        if(turnCounter>=9){
+    //            toeScore=toeScore-1
+    //        }
+    //    }
 //GIVES SCORE WHEN TIE
-      
-
-     
-    
-
        /*winning combos
        [0,1,2]
        [3,4,5]
@@ -86,24 +112,11 @@ const pieceClicked = (tile) => {
 
 
        */
-
-      
-           
-
-      
-
-   
     // clearBoard = () =>{
     //     restartButton.addEventListener('click', clear)
     //     clear=()=>{board.reset()}
 
     //     }
-    
-
-
-
-
-
 // console.log(winMes)
 
 pieces.forEach((piece) => piece.addEventListener('click', pieceClicked))

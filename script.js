51cff2c1462f107c0d88fc78e94ebe75d51820ac
aimeemisaki <aimeejmisaki@gmail.com>
2022-02-02 10:28:14 -0800

    a few edits to tic-tac-toe deliverable (WIP)

--- script.js
+++ script.js
@@ -1,129 +1,191 @@
-// what kind of variables will I need? (app state)
-  // data container (array) to hold who has gone where (the gameboard) (this would contain player choices)
-  // (optional) -- define each players choice x/o
-  // whose turn it is? -- state of player
-  // winning combinations to check (data container?)
-  // toggle/boolean if someone has won (is user allowd to click/keep playing)
-  // (cats option 1) board is full = draw
-  // (cats option 2) turn count for cats game
-
-// what will happen when the user clicks?
-  // slection of X or O (who's turn is it?)
-  // mark space if it is availible
-    // if it is marked -- do nothing
-  // check if there is a winner
-    // if there is a winner -- stop gameplay and show who won
-    // if not -- let gameplay continue
-
-// define each players chioce (x/o)    
-const xPlayer = () => {
-    // ????
-}
-
-const oPlayer = () => {
-    // ????
-}
-const player = prompt('X', 'O');
-
-if (player ==="X") {
-    xPlayer();
-} else if (player==="O") {
-    oPlayer();
-}
-
-const winningMessage = () => `Player ${currentPlayer} has won!`;
-const drawMessage = () => `Draw!`;
-const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+// Whose turn is it
+let currentPlayer = "X"
+const xPlayer = []
+const oPlayer = []
 
 
-statusDisplay.innerHTML = currentPlayerTurn();
-function handleCellPlayed() {
+const playersTurn = (event) => {
+if (currentPlayer === "X") {
+event.target.innerText = currentPlayer
+xPlayer.push(event.target.id)
+currentPlayer = "O"
+} else if (currentPlayer === "O") {
+  event.target.innerText = currentPlayer
+  oPlayer.push(event.target.id)
+  currentPlayer = "X"
 
-}
-function handlePlayerChange() {
+}}
 
-}
-function handleResultValidation() {
 
-}
-function handleCellClick() {
+// What happens when a button is clicked
+let buttons = document.querySelectorAll(".button-board")
+buttons.forEach(function(button) {
+   button.addEventListener('click', playersTurn) 
+  }) 
 
-}
-function handleRestartGame() {
+// Store where players' moves are going
+// 2 separate empty arrays (one for each player) 
+// within players function, push in ID of where they're making a move
 
-}
 
-document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
-document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
 
-function handleCellClick(clickedCellEvent) {
-     
-        const clickedCell = clickedCellEvent.target;
-    
-        const clickedCellIndex = parseInt(
-          clickedCell.getAttribute('data-cell-index')
-        );
-    
-        if (gameState[clickedCellIndex] !== "" || !gameActive) {
-            return;
-        }
-      
-        handleCellPlayed(clickedCell, clickedCellIndex);
-        handleResultValidation();
-    }
-   
-    function handleCellPlayed(clickedCell, clickedCellIndex) {
 
-        gameState[clickedCellIndex] = currentPlayer;
-        clickedCell.innerHTML = currentPlayer;
-    }
-    const winningConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-    function handleResultValidation() {
-        let roundWon = false;
-        for (let i = 0; i <= 7; i++) {
-            const winCondition = winningConditions[i];
-            let a = gameState[winCondition[0]];
-            let b = gameState[winCondition[1]];
-            let c = gameState[winCondition[2]];
-            if (a === '' || b === '' || c === '') {
-                continue;
-            }
-            if (a === b && b === c) {
-                roundWon = true;
-                break
-            }
-        }
-    if (roundWon) {
-            statusDisplay.innerHTML = winningMessage();
-            gameActive = false;
-            return;
-        }
-        let roundDraw = !gameState.includes("");
-    if (roundDraw) {
-        statusDisplay.innerHTML = drawMessage();
-        gameActive = false;
-        return;
-    }
-    handlePlayerChange();
-}
-function handlePlayerChange() {
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    statusDisplay.innerHTML = currentPlayerTurn();
-}
-function handleRestartGame() {
-    gameActive = true;
-    currentPlayer = "X";
-    gameState = ["", "", "", "", "", "", "", "", ""];
-    statusDisplay.innerHTML = currentPlayerTurn();
-    document.querySelectorAll('.cell')
-               .forEach(cell => cell.innerHTML = "");
-}
+// Winning Scenario
+// if Player "X/O" clicks any of these combos --> it is a win
+//in that case, no longer can click --> message "You won"
+
+const winningCombos = 
+[
+[0,1,2],
+[3,4,5],
+[6, 7, 8],
+[2,3,6],
+[1,4,7],
+[2,5,8],
+[0,5,9],
+[2,4,6],
+]
+
+// .includes method (built in array)
+// use for loop for winningCombos array
+//const declareWinner = () => {
+//   if (??? === winningCombos) {
+//     declareWinner.innerText = currentPlayer + "is the Winner!"
+//   }
+// }
+
+// Draw Scenario
+//if 0 remaining box --> it is a draw
+// in that case, reset 
+
+let allMoves = []
+
+// id is being pushed into the array 
+//all moves 
+// when 9 === array.length && no winner 
+
+
+
+
+// 
+// let playerNow = document.querySelector('.Current-Player')
+// let declareWinner = document.querySelector('.declareWinner')
+// let resetButton = document.querySelector('#restartButton')
+
+// //  Make the squares into an array
+// let squares = Array.from(document.querySelectorAll('.square'))
+
+// // What happens when a square is clicked
+// squares.forEach(function (square) {
+//   square.addEventListener('click', function () {
+//     if (square.innerText === '' && gameInProgress) {
+//       square.innerText = currentPlayer
+//       if (currentPlayer === 'X') {
+//         currentPlayer = 'O'
+//         playerNow.innerText = 'The current player is O'
+//       } else if (currentPlayer === 'O') {
+//         currentPlayer = 'X'
+//         playerNow.innerText = 'The current player is X'
+//       }
+//       winCondition()
+//       tieCondition()
+//     }
+//   })
+// })
+
+
+// // Winning Conditions
+// function winCondition() {
+//   if (button[0].innerText === 'X' && squares[1].innerText === 'X' && squares[2].innerText === 'X') {
+//     declareWinner.innerText = 'Player X has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[3].innerText === 'X' && squares[4].innerText === 'X' && squares[5].innerText === 'X') {
+//     declareWinner.innerText = 'Player X has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[6].innerText === 'X' && squares[7].innerText === 'X' && squares[8].innerText === 'X') {
+//     declareWinner.innerText = 'Player X has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[0].innerText === 'X' && squares[4].innerText === 'X' && squares[8].innerText === 'X') {
+//     declareWinner.innerText = 'Player X has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[1].innerText === 'X' && squares[4].innerText === 'X' && squares[7].innerText === 'X') {
+//     declareWinner.innerText = 'Player X has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[2].innerText === 'X' && squares[5].innerText === 'X' && squares[8].innerText === 'X') {
+//     declareWinner.innerText = 'Player X has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[0].innerText === 'X' && squares[4].innerText === 'X' && squares[8].innerText === 'X') {
+//     declareWinner.innerText = 'Player X has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[2].innerText === 'X' && squares[4].innerText === 'X' && squares[6].innerText === 'X') {
+//     declareWinner.innerText = 'Player X has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[0].innerText === 'O' && squares[1].innerText === 'O' && squares[2].innerText === 'O') {
+//     declareWinner.innerText = 'Player O has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[3].innerText === 'O' && squares[4].innerText === 'O' && squares[5].innerText === 'O') {
+//     declareWinner.innerText = 'Player O has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[6].innerText === 'O' && squares[7].innerText === 'O' && squares[8].innerText === 'O') {
+//     declareWinner.innerText = 'Player O has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[0].innerText === 'O' && squares[3].innerText === 'O' && squares[6].innerText === 'O') {
+//     declareWinner.innerText = 'Player O has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[1].innerText === 'O' && squares[4].innerText === 'O' && squares[7].innerText === 'O') {
+//     declareWinner.innerText = 'Player O has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[3].innerText === 'O' && squares[5].innerText === 'O' && squares[8].innerText === 'O') {
+//     declareWinner.innerText = 'Player O has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[0].innerText === 'O' && squares[4].innerText === 'O' && squares[8].innerText === 'O') {
+//     declareWinner.innerText = 'Player O has won'
+//     playerNow.innerText = 'Game Over'
+//     gameInProgress = false
+//   } else if (squares[2].innerText === 'O' && squares[4].innerText === 'O' && squares[6].innerText === 'O') {
+//     declareWinner.innerText = 'Player O has won'
+//     gameInProgress = false
+//     playerNow.innerText = 'Game Over'
+//   }
+// }
+
+
+// //  Tie Condition
+// function tieCondition() {
+//   let filledSquare = 0
+//   for (let i = 0; i < squares.length; i++) {
+//     if (squares[i].innerText === 'X' || squares[i].innerText === 'O') {
+//       filledSquare++
+//     }
+//   }
+//   if (filledSquare === 9) {
+//     declareWinner.innerText = 'Draw'
+//     playerNow.innerText = 'Game Over'
+//   }
+// }
+
+
+// //  Reset condition + button press
+// resetButton.addEventListener('click', function () {
+//   for (let i = 0; i < squares.length; i++) {
+//     squares[i].innerText = ''
+//     playerNow.innerText = ''
+//     declareWinner.innerText = ''
+//     gameInProgress = true
+//     currentPlayer = 'X'
+//     playerNow.innerText = 'The current player is X'
+// } })

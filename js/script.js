6dcdb85af59cadf6eebce26f6ebd447e6c3552fa
Billy Lu <billyfwlu@gmail.com>
2022-05-01 22:56:46 -0700

    Added simple AI feature

--- js/script.js
+++ js/script.js
@@ -1,87 +1,156 @@
 const playerTurnText = document.querySelector("#playerTurn")
+const clearButton = document.querySelector("#clearButton")
+const oWinCountText = document.querySelector("#oWinCount")
+const xWinCountText = document.querySelector("#xWinCount")
+const tieCountText = document.querySelector("#tieCount")
+const vsCompButton = document.querySelector("#vsComp-Button")
+const playerXspan = document.querySelector("#playerXspan")
+let playerVsText = document.querySelector("#playerVsText")
+let btns = document.querySelectorAll('.btns')
+
+let playerO = [false, false, false, false, false, false, false, false, false]
+let playerX = [false, false, false, false, false, false, false, false, false]
+let gameArr = [false, false, false, false, false, false, false, false, false]
+
+const Oturn = "Player: O's Turn"
+const Xturn = "Player: X's Turn"
+
+let tieText = "No One won, Clear to try again"
+let clickNum = 0
+let playerChoice = "O"
+let currPlayerArr = playerO
+let playerOwins = 0
+let playerXwins = 0
+let tieCounter = 0
+let randy
+let playVScomp = false
+let winFlag = false
 
 document.addEventListener('DOMContentLoaded', function() {
-    
-    let btns = document.querySelectorAll('.btns');
-    const clearButton = document.querySelector("#clearButton")
-    const oWinCountText = document.querySelector("#oWinCount")
-    const xWinCountText = document.querySelector("#xWinCount")
-    const tieCountText = document.querySelector("#tieCount")
-    
-    let playerX = [false, false, false, false, false, false, false, false, false]
-    let playerO = [false, false, false, false, false, false, false, false, false]
-
-    const Oturn = "Player: O's Turn"
-    const Xturn = "Player: X's Turn"
-
-    let tieText = "No One won, Clear to try again"
-    
-    let clickNum = 0
-    let playerChoice = "O"
-    let game = playerO
-    let winFlag = 0
-    let playerOwins = 0
-    let playerXwins = 0
-    let tieCounter = 0
-    let randy = Math.floor(Math.random() * 10);
-    console.log("Random Number:", randy)
 
     btns.forEach(function(i){
         i.addEventListener("click", function(event){
+        // console.log("-----------------------------------")
+
             const play = event.target
             if (play.innerText == "") {
                 play.innerText = playerChoice
                 
                 if (playerChoice == "O"){
                     playerO[play.value-1] = true
-                    game = playerO
+                    gameArr[play.value-1] = true
+                    i.disabled = true
+                    currPlayerArr = playerO
                     clickNum++
+                    winCheck(playerChoice, currPlayerArr, clickNum)
                 } 
                 
                 if(playerChoice == "X") {
                     playerX[play.value-1] = true
-                    game = playerX
+                    gameArr[play.value-1] = true
+                    i.disabled = true
+                    currPlayerArr = playerX
                     clickNum++
+                    winCheck(playerChoice, currPlayerArr, clickNum)
                 }
                 console.log("clickNum:",clickNum)
-                winFlag = winCheck(game)
+                console.log("player on click:",playerChoice)
+                
 
-                //checks to see if someone wins, switches to next player if not and sets tie at 9 turns.
-                if (winFlag == 1) {
-                    playerTurnText.innerText = "Player " + playerChoice + " wins"
-                    disButtons(btns)
-
-                    if (playerChoice == "O") {
-                        playerOwins++
-                        oWinCountText.innerText = playerOwins
-                    } else if (playerChoice == "X"){
-                        playerXwins++
-                        xWinCountText.innerText = playerXwins
-                    }
-
-                } else if (clickNum < 9 && winFlag != 1) {
-                    playerChoice = playerSwitcher(playerChoice)
-                } else if (clickNum == 9){
-                    playerTurnText.innerText = tieText
-                    disButtons(btns)
-                    tieCounter++
-                    tieCountText.innerText = tieCounter
-                }
             }
+            console.log("play vs comp:", playVScomp)
+        
+            if (playVScomp == true && clickNum < 8 && winFlag == false) {
+                playerChoice = playerSwitcher(playerChoice)
+                compAIturn(playerChoice, gameArr)
+                currPlayerArr = playerX
+                winCheck(playerChoice, currPlayerArr, clickNum)
+                playerChoice = playerSwitcher(playerChoice)
+
+            } else if (playVScomp == false && winFlag == false){
+                playerChoice = playerSwitcher(playerChoice)
+            }
+    
         })
-    }) 
+    })
+
+function reset() {
+    enButtons(btns)
+    playerX = [false, false, false, false, false, false, false, false, false]
+    playerO = [false, false, false, false, false, false, false, false, false]
+    gameArr = [false, false, false, false, false, false, false, false, false]
+    clickNum = 0
+    playerTurnText.innerHTML = Oturn
+    playerChoice = "O"
+    winFlag = false
+}
 
     clearButton.addEventListener('click', function(){
-        enButtons(btns)
-        playerX = [false, false, false, false, false, false, false, false, false]
-        playerO = [false, false, false, false, false, false, false, false, false]
-        clickNum = 0
-        document.querySelector("#playerTurn").innerHTML = Oturn
-        playerChoice = "O"
+        reset()
     })
+
+    vsCompButton.addEventListener("click", function(){
+        console.log("comp button pushed")
+        console.log("vs computer? before switch:", playVScomp)
+        if (playVScomp == true) {
+            playVScomp = false
+            
+            vsCompButton.innerText = "Player Vs Computer"
+            playerXspan.innerText = "Player X"
+        } else {
+            playVScomp = true
+            
+            vsCompButton.innerText = "Player Vs Player"
+            playerXspan.innerText = "Computer"
+            
+        }
+        reset()
+        playerXwins = 0
+        playerOwins = 0
+        tieCounter = 0
+        oWinCountText.innerText = playerOwins
+        xWinCountText.innerHTML = playerXwins
+        tieCountText. innerHTML = tieCounter
+        console.log("vs computer? after switch:", playVScomp)
+    })
+
 })
 
-function winCheck (arr) {
+function getRand(Array) {
+    g =  Math.floor(Math.random() * (8 - 0) + 0)
+    randCounter = 0
+    while(Array[g] == true) {
+        g =  Math.floor(Math.random() * (8 - 0) + 0)
+        randCounter++
+    }
+    randCounter = 0
+    return g
+}
+
+
+function compAIturn(playerChoice,gameArr,randy) {
+    console.log("compAIturn running")
+    randy = getRand(gameArr)
+    for (i = 0; i<9;i++) {
+        if (playerX[randy] == false) {
+            btns.forEach(function(i) {
+                if (i.value - 1 == randy) {
+                    i.innerText = playerChoice
+                    gameArr[randy] = true
+                    playerX[randy] = true
+                    console.log("Player X arr:",playerX)
+                    i.disabled = true
+                }                
+            })
+        }
+
+    }
+    // console.log("Final gameArr:",gameArr)
+    // console.log("Final player X:",playerX)
+    clickNum++
+}
+
+function winCheck (playerChoice, arr,clickNum) {
     let count = 0
     const winArr = [
         [true, true, true, "", "", "", "", "", ""],
@@ -96,17 +165,54 @@ function winCheck (arr) {
     
     // count 
     for (i=0; i < winArr.length ; i++){
+        
+
         for (j=0; j < winArr[i].length ;j++){
             if (winArr[i][j] === true && arr[j] === true) {               
                 count++
                 if (count === 3) {
-                    return 1                    
+                    winFlag = true                    
                 }
             }
         }
         count = 0
     }
-    return 0
+
+    //checks to see if someone wins, switches to next player if not and sets tie at 9 turns.
+    if (winFlag == true) {
+        console.log("Current Player", playerChoice)
+        console.log("click number:",clickNum)
+        if (playVScomp == false) {
+            playerTurnText.innerText = "Player " + playerChoice + " wins"
+        } else if (playVScomp == true) {
+            if (playerChoice == "X"){
+                playerTurnText.innerText = "Computer wins"
+            } else {
+                playerTurnText.innerText = "Player " + playerChoice + " wins"
+            }
+        } 
+
+        
+        
+        disButtons(btns)
+
+        if (playerChoice == "O") {
+            console.log("O wins flag")
+            playerOwins++
+            oWinCountText.innerText = playerOwins
+        } else if (playerChoice == "X"){
+            console.log("X wins flag")
+            playerXwins++
+            xWinCountText.innerText = playerXwins
+        } 
+
+    } else if (winFlag == false && clickNum == 9){
+        console.log("tie flag")
+    playerTurnText.innerText = tieText
+    disButtons(btns)
+    tieCounter++
+    tieCountText.innerText = tieCounter
+    }
 }
 
 function playerSwitcher(turn) {
@@ -117,7 +223,7 @@ function playerSwitcher(turn) {
         turn = "X"
         playerTurnText.innerText = "Player: X's turn"
     }
-   
+
     return turn
 }
 
@@ -132,6 +238,5 @@ function enButtons(btns) {
     btns.forEach(function(i){
         i.disabled = false
         i.innerText = ""
-        playerChoice = "O"
     })
 }

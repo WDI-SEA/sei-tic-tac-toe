cf3a828f94d030c6adafbce132a5d8feb78c880a
Manion-sebastian <Sjmanion123@gmail.com>
2022-08-13 11:33:31 -0700

    added sidebar functionality. working on logic.

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+// logic
+let turn = 0
+let turnButton = document.getElementById('turnButton')
+let clearButton = document.getElementById('clearButton')
+let numSpan = document.getElementById('turnNumSpan')
+
+turnButton.addEventListener('click', function() {
+    console.log('hello')
+    turn++
+    numSpan.innerText = turn
+    if(turn === 8) [
+        turnButton.innerText = "End Game"
+    ]
+    if(turn > 9) {
+        alert('game over')
+    }
+})
+
 
 
 
 // if clicked
 let divs = document.querySelectorAll(".grid-item")
 const plotClick = e => {
-    e.currentTarget.style.borderRadius = "100%"
+    // e.currentTarget.classList.toggle('clicked')
+    if (turn % 2 === 0) {
+        e.currentTarget.innerText = "X"
+    } else {
+        e.currentTarget.innerText = "O"
+    }
+
 }
 divs.forEach(div => {
     div.addEventListener('click', plotClick)

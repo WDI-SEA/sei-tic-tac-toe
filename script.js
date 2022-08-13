8b7cd278f4aaed98314ff74a5da41f260c96a8fc
prijacash <prijacash@gmail.com>
2022-08-12 18:41:03 -0700

    Prija - added more structure and framing.

---
+++ script.js
@@ -0,0 +1,54 @@
+console.log('test')
+
+// APP STATE (variables)
+    // a variable to track whose turn it is
+    // a var for 'x' and a var for 'o'
+    // a var to keep count of moves to detect cats/draw game
+    // gameBoard array to keep track of player moves ['x', '', 'o', 'x', '', 'o', 'x', '', '']
+        // 2d array
+        // [
+        //     ['x', '', 'o'],
+        //     ['x', 'o', ''],
+        //     ['x', '', 'o']
+        // ]
+    // a boolean for if the game is over or not -- is the game currently happening?
+
+
+// EVENT LISTENERS
+    // click event listener(s) for player clicks
+        // change innertext of div to X or O
+        // store the player's move in the gameBoard array
+        // change to the next player's turn
+        // check for a win -- calling a win condition function/doing all the win logic
+    // click event to clear/reset the board
+
+// some way to tell if a player has already clicked on a square
+// win/tie game logic in functions
+    // option: detect a win using conditionals and comparisions 
+    // option hard mode: store all winning conditions and use loops to compare them to the gameBoard array
+        // if the game is won, display the winner and prevent the users from clicking more
+        // if it is a cats game, display that and prevent clicks
+
+
+// DELIVERABLE READ OUT
+
+// GETTING STARTED
+// Before you even start working with JavaScript, construct the gameboard. The gameboard page should include the 3x3 grid (of divs), and at minimum a reset button. Using id and/or class on clickable elements will help you wire this up in JavaScript afterwards.
+// The JavaScript portion will be next
+// Select elements and attach functions via event listeners
+// You will also need a variable to keep track of moves. This will be used to indicate whether or not to draw an X or an O
+
+// REQUIREMENTS
+// A user should be able to click on different squares to make a move.
+// Every click will alternate between marking an X and O
+// Upon marking of an individual cell, use JavaScript to add an X or O to the cell, according to whose turn it is.
+// A cell should not be able to be replayed once marked.
+// You should not be able to click remaining empty cells after the game is over.
+// Add a reset button that will clear the contents of the board.
+// Display a message to indicate which turn is about to be played.
+// Detect draw conditions (ties/cat's game)
+// Detect winner: Stop game and declare the winner if one player ends up getting three in a row.
+// Hint: Determine a set of winning combinations. Check those combinations on the board contents after every move.
+// Have Fun - The best way to learn is by playing with code. Let creativity guide you and try some experiments with JS and CSS and see what you can do.
+
+// */

2de88f8121927dc23c6a1b7034b67654bdaffe97
elek8871 <elekmichelle@gmail.com>
2022-08-13 12:10:20 -0600

    adds framework for js

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
-console.log("test")
+// take a deep breath. you can do this!
+
+// VARIABLES
+/*
+variable for user turns
+x
+o
+variable to keep track of moves, detect win/lose/draw
+gameboard array to keep track of player moves
+boolen for end game
+*/
+
+// Event Listeners
+// change inner text to x or o : getElementbyID:"box1"...
+// store players move and keep box from being chosen again
+// change tunrs
+// check for win/lose/draw
+// reset the game

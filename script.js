c024241b80e68316f41c07a97f64696431a28e85
tatebonham <tatebonham@gmail.com>
2022-08-13 11:16:19 -0700

    Refactored the gameBoard variable, added the DomContentLoaded eventListener, added the reset button, created variables for every space in the grid.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // APP STATE (variables)
-    // a variable to track whose turn it is
-let startingPlayer = 'x'    
-let currentPlayer =  startingPlayer
 
-    // a var for 'x' and a var for 'o'
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+   
+     // a variable to track whose turn it is
+     // a var for 'x' and a var for 'o'
 let x = 'x'
 let o = 'o'
+let currentPlayer =  x
+
+   
+
 
     // a var to keep count of moves to detect cats/draw game
         /* Add your logic here */
@@ -13,9 +18,12 @@ let count = 9
 let catsGame = () => {
     if(count === 9){
         document.getElementById("h2").innerHTML = "Cat\'s Game";
+        
     }
 }
-setTimeout(catsGame, 2000)
+
+
+// setTimeout(catsGame, 2000)
 
     // gameBoard array to keep track of player moves ['x', '', 'o', 'x', '', 'o', 'x', '', '']
         // 2d array
@@ -24,17 +32,26 @@ setTimeout(catsGame, 2000)
         //     ['x', 'o', ''],
         //     ['x', '', 'o']
         // ]
-let gameBoard = [
-    [document.getElementById('aOne'), document.getElementById('bOne'), document.getElementById('cOne')],
-    [document.getElementById('aTwo'), document.getElementById('bTwo') , document.getElementById('cTwo') ],
-    [document.getElementById('aThree'), document.getElementById('bThree') , document.getElementById('cThree') ]
-]
-    // a boolean for if the game is over or not -- is the game currently happening?
-// let over = (gameState) => {
-//     if(gameState === true){
+let gameBoard = ['','','','','','','','','']
 
-//     }
-// }
+let aOne = document.getElementById("aOne");
+let bOne = document.getElementById("bOne");
+let cOne = document.getElementById("cOne");
+let aTwo = document.getElementById("aTwo");
+let bTwo = document.getElementById("bTwo");
+let cTwo = document.getElementById("cTwo");
+let aThree = document.getElementById("aThree");
+let bThree = document.getElementById("bThree");
+let cThree = document.getElementById("cThree");
+
+    // a boolean for if the game is over or not -- is the game currently happening?
+
+let gameOver = false;
+
+let reset = document.querySelector('button')
+reset.addEventListener('click', () => {
+    console.log('ugh')
+})
 
 // EVENT LISTENERS
     // click event listener(s) for player clicks
@@ -44,9 +61,12 @@ let gameBoard = [
         // check for a win -- calling a win condition function/doing all the win logic
     // click event to clear/reset the board
 
+
 // some way to tell if a player has already clicked on a square
 // win/tie game logic in functions
     // option: detect a win using conditionals and comparisions 
     // option hard mode: store all winning conditions and use loops to compare them to the gameBoard array
         // if the game is won, display the winner and prevent the users from clicking more
         // if it is a cats game, display that and prevent clicks
+
+});

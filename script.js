a9481592f27a483fb5c5ff3d9e064e9aecfe749e
Manion-sebastian <Sjmanion123@gmail.com>
2022-08-13 10:06:47 -0700

    added new eL fixed scope issue

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 
-let clicked = document.addEventListener('click', function(e) {
-    console.log(e.target.id)
-    e.target.classList.toggle('clicked')
 
+
+// if clicked
+let divs = document.querySelectorAll(".grid-item")
+const plotClick = e => {
+    e.currentTarget.style.borderRadius = "100%"
+}
+divs.forEach(div => {
+    div.addEventListener('click', plotClick)
 })
+
+// if win
+// let win = true
+// function winScreen() {
+//     if(win === true) {
+//         document.getElementById('a1').toggle('winner')
+//     }
+// }
+
+
+// winScreen()

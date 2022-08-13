80cb2c1384cc5f70462de7423ecb8e4a752b8d7b
elek8871 <elekmichelle@gmail.com>
2022-08-13 17:50:47 -0600

    fixes js script issue

--- game.js
+++ game.js
@@ -9,11 +9,22 @@ gameboard array to keep track of player moves
 boolen for end game
 */
 
- let userTurn = ""
+let boxOne = "one"
+let boxTwo= "two"
+let boxThree = "three"
+let boxFour = "four"
+let boxFive = "five"
+let boxSix = "six"
+let boxSeven= "seven"
+let boxEight = "eight"
+let boxNine = "nine"
 
-const gameBoxes = ["box1", "box2","box3","box4","box5","box6","box7","box8", "box9"]
-const userOne= "x"
-const userTwo= "O"
+
+
+let gameBoxes = ""
+const userOne= ""
+const userTwo= ""
+
 
 //  game logic
 
@@ -43,13 +54,31 @@ const userTwo= "O"
 // Event Listeners
 // change inner text to x or o : getElementbyID:"box1"...
 
-document.getElementById("box1").addEventListener("click", playerChoiceX);
+let choice= document.getElementById("six")
 
-function playerChoiceX(){
-    document.getElementById("box1").innerText= "x";
-}
+choice.addEventListener("click", playerChoiceX);
+
+ function playerChoiceX(){ 
+        document.getElementById("six").innerText= "x";
+        let boxSix = X
+ }
+
+  function choice {
+    playerChoiceX ()
+// userOne (X) chooses then 
+//games goes to player 2 
+// userTwo chooses (O) 
+    
+// game goes to userOne(X)
+  }
 
 // store players move and keep box from being chosen again
-// change tunrs
+
+
+// change turns
+
+
 // check for win/lose/draw
+
 // reset the game
+// 

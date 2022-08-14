b65d83a08cd09975da84cfe3df978feda038b584
waltersmatthew <waltersmatthew88@gmail.com>
2022-08-14 00:31:36 -0700

    fix CSS lettering and add result array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // APP STATE (variables)
 // a variable to track whose turn it is
+    // %2 = 0, or %2 = 1
 let playerTurn = 0
 // a var for 'x' and a var for 'o'
 let X = "X"
@@ -7,7 +8,7 @@ let O = "O"
 let clicked = false
 // a var to keep count of moves to detect cats/draw game
 // gameBoard array to keep track of player moves ['x', '', 'o', 'x', '', 'o', 'x', '', '']
-let gameBoard = []
+let gameBoard = ['', '', '', '', '', '', '', '', '']
 // a boolean for if the game is over or not -- is the game currently happening?
 let gameOver = false
 //Variables for game slots
@@ -65,99 +66,124 @@ boxOne.addEventListener("click", function(e){
         oneText.innerText = X
         } else{
             oneText.innerText = O
-        }
-    }
+        }    
     playerTurn++
     boxOneClick = true   
-    console.log(boxOneClick)
+    gameBoard.splice(0, 1, oneText.innerText)
+    console.log(gameBoard)
+}
 })
 boxTwo.addEventListener("click", function(e){
     if(!boxTwoClick){
         if(playerTurn % 2 == 0){
-        twoText.innerText = X
+            twoText.innerText = X
         } else{
             twoText.innerText = O
         }
-    }
     playerTurn++
     boxTwoClick = true
+    gameBoard.splice(1, 1, twoText.innerText)
+    console.log(gameBoard)
+    }
+
 })
 boxThree.addEventListener("click", function(e){
     if(!boxThreeClick){
         if(playerTurn % 2 == 0){
-        threeText.innerText = "X"
+        threeText.innerText = X
         } else{
-            threeText.innerText = "O"
-        }
-    }
+            threeText.innerText = O
+        }    
     playerTurn++
-    boxThreeClick = true   
+    boxThreeClick = true 
+    gameBoard.splice(2, 1, threeText.innerText)
+    console.log(gameBoard)
+    }
+  
 })
 boxFour.addEventListener("click", function(e){
     if(!boxFourClick){
         if(playerTurn % 2 == 0){
-        fourText.innerText = "X"
+        fourText.innerText = X
         } else{
-            fourText.innerText = "O"
+            fourText.innerText = O
         }
-    }
     playerTurn++
-    boxFourClick = true   
+    boxFourClick = true
+    gameBoard.splice(3, 1, fourText.innerText)
+    console.log(gameBoard)
+    }
+      
 })
 boxFive.addEventListener("click", function(e){
     if(!boxFiveClick){
         if(playerTurn % 2 == 0){
-        fiveText.innerText = "X"
+        fiveText.innerText = X
         } else{
-            fiveText.innerText = "O"
-        }
-    }
+            fiveText.innerText = O
+        }   
     playerTurn++
-    boxFiveClick = true   
+    boxFiveClick = true 
+    gameBoard.splice(4, 1, fiveText.innerText)
+    console.log(gameBoard)
+    }
+   
 })
 boxSix.addEventListener("click", function(e){
     if(!boxSixClick){
         if(playerTurn % 2 == 0){
-        sixText.innerText = "X"
+        sixText.innerText = X
         } else{
-            sixText.innerText = "O"
-        }
-    }
+            sixText.innerText = O
+        }    
     playerTurn++
     boxSixClick = true
+    gameBoard.splice(5, 1, sixText.innerText)
+    console.log(gameBoard)
+    }
+
 })
 boxSeven.addEventListener("click", function(e){
     if(!boxSevenClick){
         if(playerTurn % 2 == 0){
-        sevenText.innerText = "X"
+        sevenText.innerText = X
         } else{
-            sevenText.innerText = "O"
-        }
-    }
+            sevenText.innerText = O
+        }    
     playerTurn++
     boxSevenClick = true
+    gameBoard.splice(6, 1, sevenText.innerText)
+    console.log(gameBoard)
+    }
+
 })
 boxEight.addEventListener("click", function(e){
     if(!boxEightClick){
         if(playerTurn % 2 == 0){
-        eightText.innerText = "X"
+        eightText.innerText = X
         } else{
-            eightText.innerText = "O"
+            eightText.innerText = O
         }
-    }
     playerTurn++
     boxEightClick = true
+    gameBoard.splice(7, 1, eightText.innerText)
+    console.log(gameBoard)
+    }
+
 })
 boxNine.addEventListener("click", function(e){
     if(!boxNineClick){
         if(playerTurn % 2 == 0){
-        nineText.innerText = "X"
+        nineText.innerText = X
         } else{
-            nineText.innerText = "O"
-        }
-    }
+            nineText.innerText = O
+        }  
     playerTurn++
     boxNineClick = true
+    gameBoard.splice(8, 1, nineText.innerText)
+    console.log(gameBoard)
+    }
+  
 })
             // change innertext of div to X or O
             // store the player's move in the gameBoard array
@@ -174,5 +200,14 @@ resetBtn.addEventListener("click", function(e){
     sevenText.innerText = ""
     eightText.innerText = ""
     nineText.innerText = ""
+    boxOneClick = false
+    boxTwoClick = false
+    boxThreeClick = false
+    boxFourClick = false
+    boxFiveClick = false
+    boxSixClick = false
+    boxSevenClick = false
+    boxEightClick = false
+    boxNineClick = false
 })
 

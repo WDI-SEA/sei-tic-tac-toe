4592acc321cc16897eb1df408df4a91d4c57f767
valerieyang00 <valerieyang00@gmail.com>
2022-08-13 18:16:58 -0700

    finished homework

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-// APP STATE (variables)
-    // a variable to track whose turn it is
-    // a var for 'x' and a var for 'o'
-    // a var to keep count of moves to detect cats/draw game
-    // gameBoard array to keep track of player moves ['x', '', 'o', 'x', '', 'o', 'x', '', '']
-        // 2d array
-        // [
-        //     ['x', '', 'o'],
-        //     ['x', 'o', ''],
-        //     ['x', '', 'o']
-        // ]
-    // a boolean for if the game is over or not -- is the game currently happening?
 
-
-
-// EVENT LISTENERS
-    // click event listener(s) for player clicks
-        // change innertext of div to X or O
-        // store the player's move in the gameBoard array
-        // change to the next player's turn
-        // check for a win -- calling a win condition function/doing all the win logic
-    // click event to clear/reset the board
-
+//Set all const variables
 const sq1 = document.getElementById("sq1")
 const sq2 = document.getElementById("sq2")
 const sq3 = document.getElementById("sq3")
@@ -31,10 +10,9 @@ const sq7 = document.getElementById("sq7")
 const sq8 = document.getElementById("sq8")
 const sq9 = document.getElementById("sq9")
 const grid = document.getElementsByTagName("div")
-
 const reset = document.getElementById("reset")
 
-//
+//Set Win arrays
 const win = [
     [sq1, sq2, sq3],
     [sq4, sq5, sq6],
@@ -46,23 +24,25 @@ const win = [
     [sq1, sq5, sq9],
 ]
 
-
+//Set all lets
 let displayText = document.querySelector("p")
-
 let clickedSqsO = []
 let clickedSqsX = []
 let clickedSqsTotal = []
-
-const player = ["O","X"]
+let player = ["O","X"]
 let turn = player[0]
 
+//Function to check results
 
 function checkWinForO () {
     for (i=0; i < win.length; i++) {
         let resultForO = win[i].every(v => clickedSqsO.includes(v));
         console.log(resultForO)
-        if (resultForO === true) {
+        if (resultForO === true) { 
             displayText.innerText = `Player ${player[0]} WINS!`
+            for (i=0; i < grid.length; i++) {
+                grid[i].style.pointerEvents = "none";}
+            break            
         } else {
             turn = player[1]
             displayText.innerText = `Player ${turn}, It's your turn!`}}}
@@ -72,20 +52,28 @@ function checkWinForX () {
     for (i=0; i < win.length; i++) {
         let resultForX = win[i].every(v => clickedSqsX.includes(v));
         console.log(resultForX)
-        if (resultForX === true) {
+        if (resultForX[i] === true) {
             displayText.innerText = `Player ${player[1]} WINS!`
+            for (i=0; i < grid.length; i++) {
+                grid[i].style.pointerEvents = "none";}
+            break
         } else {
             turn = player[0]
             displayText.innerText = `Player ${turn}, It's your turn!`}}}
 
+// Event Listeners
 
 for (let i=0; i < grid.length; i++) {
     grid[i].addEventListener("click", function (e) {
     grid[i].innerText = turn;
     grid[i].style.pointerEvents = "none";
     clickedSqsTotal.push(grid[i]);
+    if (turn === player[0]) {
+        grid[i].style.backgroundColor = "#FFA07A"
+    } else {
+        grid[i].style.backgroundColor = "#9FE2BF"}
     console.log(clickedSqsTotal)
-    if (clickedSqsTotal === 9) {displayText.innerText = "It's a draw! Rematch?"}
+    if (clickedSqsTotal.length === 9) {displayText.innerText = "It's a draw! Rematch?"}
     else {
         if (turn === player[0]) {
         clickedSqsO.push(grid[i])
@@ -101,8 +89,14 @@ for (let i=0; i < grid.length; i++) {
 
 reset.addEventListener("click", function () {
     for(i=0; i < grid.length; i++) {
+        grid[i].style.pointerEvents = "auto"
         grid[i].innerText = ""
+        grid[i].style.backgroundColor = "burlywood"
     }
     displayText.innerText = "Play Again! 👍"
+    clickedSqsO = []
+    clickedSqsX = []
+    clickedSqsTotal = []        
+    turn = player[0]
 })
 

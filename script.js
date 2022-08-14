cedea71fa7de5c2d6de6a509ef797743a3889169
tpalmer111D <timpalmer@Tims-MacBook-Pro.local>
2022-08-14 16:14:35 -0600

    cleaned up notes and broke game function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Tic-Tac-Toe notes
+//___________________
+
+// arrays representing each indivual win combo
 const winningComboOne = ["buttonOne", "buttonTwo", "buttonThree"]
 const winningComboTwo = ["buttonOne", "buttonFour", "buttonSeven"]
 const winningComboThree = ["buttonOne", "buttonFive", "buttonNine"]
@@ -7,8 +11,10 @@ const winningComboSix = ["buttonThree", "buttonFive", "buttonSeven"]
 const winningComboSeven = ["buttonThree", "buttonSix", "buttonNine"]
 const winningComboEight = ["buttonSeven", "buttonEight", "buttonNine"]
 
+// single array of all winning combinations
 const winningCombos = [winningComboOne, winningComboTwo, winningComboThree, winningComboFour, winningComboFive, winningComboSix, winningComboSeven, winningComboEight]
 
+// var for each gameboard button 
 let butOne = document.getElementById("buttonOne")
 let butTwo = document.getElementById("buttonTwo")
 let butThree = document.getElementById("buttonThree")
@@ -19,18 +25,28 @@ let butSeven = document.getElementById("buttonSeven")
 let butEight = document.getElementById("buttonEight")
 let butNine = document.getElementById("buttonNine")
 
+// single array for buttons representing the game board
 const buttons = [butOne, butTwo, butThree, butFour, butFive, butSix, butSeven, butEight, butNine]
 
+// var for reset button
 let butNew = document.getElementById("resetButton")
 
-let playerOneTurns = [], playerTwoTurns = []  // Receives button id from clicks
+// Receives button id from clicks
+let playerOneTurns = [], playerTwoTurns = []  
 
-let turnCount = 0 /* number of total clicks*/
+// number of total wins
+let playerOneWins = 0, playerTwoWins = 0 
 
+// number of total clicks, per player
+let turnCount = 0
+
+// var to get displayBox by id 
 const displayBox = document.getElementById("displayBox")
 
+// test: doesn't test anything really. 
 console.log("Hello")
 
+// function that runs when end game conditions are met
 function endGame(player) {
     buttons.forEach(function(item) {
         item.disabled = true })
@@ -41,16 +57,19 @@ function endGame(player) {
         }
         case 1: {
             displayBox.innerText = "Player one wins!"
+            playerOneWins ++
+            displayBoxTwo.innerText = playerOneWins
             break
         } 
         case 2: {
             displayBox.innerText = "Player two wins!"
+            playerTwoWins ++
+            displayBoxThree.innerText = playerTwoWins
             break
         }
     }
 }
-
-
+// function to compare players array to win combos (solution found on grepper)
 const compareArrays = (a, b) => {
     if (a.length < b.length) {
         return false
@@ -62,13 +81,13 @@ const compareArrays = (a, b) => {
   )
 }
 
-// Other code blocks tested here moved to the bottom of the page.
-
+// function to run everytime gameboard buttons are clicked
+// ("& 1" checks if the last digit of the number as binary is 1)
 function turnProcess(e) {
         displayBox.innerText = "Player One's Turn"
         turnCount ++
     let button = e.srcElement
-    if (turnCount & 1) {  //("& 1" checks if the last digit of the number as binary is 1)
+    if (turnCount & 1) {  
         button.innerText = "X"
         playerOneTurns.push(button.id)
         console.log(playerOneTurns)
@@ -81,27 +100,22 @@ function turnProcess(e) {
 
 
     }
-    button.disabled = true // Makes button not clickable any more
+// makes event button not clickable any more
+    button.disabled = true
     
+// calls compareArrays and compares against win condtions + tie
     winningCombos.forEach(function(item) {
         console.log(item)
-        if (turnCount == 9) {
-            endGame(0)//You SUCK IT IS A TIE!!
-        } else if (compareArrays(playerOneTurns, item)) {
-            endGame(1)// Run win conditon: PLAYER 1 WINS
+        if (compareArrays(playerOneTurns, item)) {
+            endGame(1)// PLAYER 1 WINS
         } else if (compareArrays(playerTwoTurns, item)) {
-            endGame(2)// run win condition PLAYER 2 WINS
-        } 
+            endGame(2)// PLAYER 2 WINS
+        } else if (turnCount == 9) {
+            endGame(0)//IT IS A TIE!!
+        }
     })
     
-    
-} 
-/*
-buttons.forEach(function(item) {
-    item.addEventListener("click", turnProcess)
-})
-*/
-
+// adds event listener to each gameboard button and calls turnProcess 
 butOne.addEventListener("click", turnProcess)
 butTwo.addEventListener("click", turnProcess)
 butThree.addEventListener("click", turnProcess)
@@ -112,8 +126,7 @@ butSeven.addEventListener("click", turnProcess)
 butEight.addEventListener("click", turnProcess)
 butNine.addEventListener("click", turnProcess)
 
-
-
+// reset button event listener and anon function to reset board
 butNew.addEventListener("click", function() {
     buttons.forEach(function(item) {
         item.disabled = false
@@ -122,10 +135,19 @@ butNew.addEventListener("click", function() {
     playerOneTurns = []
     playerTwoTurns = []
     turnCount = 0
-    displayBox.innerText = "" //some ""
+    displayBox.innerText = ""
 })
 
 
+
+
+
+
+
+
+//______________________________________________
+//NOTES SECTION---------------------------------
+//notes below this line are only kept for reference durng the process and to preserve said process. In the future these notes would be deleted or saced somewhere else.
 
 // APP STATE (variables)
     // a variable to track whose turn it is
@@ -236,4 +258,34 @@ function compareArrays(a, b) {
     return (newA.length == 0 && newB.length == 0)
     
 }
+*/
+
+/*
+const compareArrays = (a, b) => {
+    return (
+        a.every((el) => b.includes(el))
+    )
+}
+*/
+
+
+
+    /*
+    winningCombos.forEach(function(item) {
+        console.log(item)
+        if (turnCount == 9) {
+            endGame(0)//You SUCK IT IS A TIE!!
+        } else if (compareArrays(playerOneTurns, item)) {
+            endGame(1)// Run win conditon: PLAYER 1 WINS
+        } else if (compareArrays(playerTwoTurns, item)) {
+            endGame(2)// run win condition PLAYER 2 WINS
+        } 
+    })
+    */
+    
+} 
+/*
+buttons.forEach(function(item) {
+    item.addEventListener("click", turnProcess)
+})
 */

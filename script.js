264cc851da65b7e059dafe85af88cb43ef62f8cf
tatebonham <tatebonham@gmail.com>
2022-08-14 16:23:21 -0700

    set up win conditions, gave the event listeners more functions

--- script.js
+++ script.js
@@ -54,6 +54,8 @@ let aThreeSpace = true
 let bThreeSpace = true
 let cThreeSpace = true
 
+
+
 let gameBoard = ['','','','','','','','','']
 
     // a boolean for if the game is over or not -- is the game currently happening?
@@ -81,28 +83,130 @@ let changePlayers = () => {
     }
 }
 
+
+// let gameBoard = [
+// '0','1','2',
+// '3','4','5',
+// '6','7','8']
+let winConditions = () => {
+    if(currentPlayer === gameBoard[0] && currentPlayer === gameBoard[1] && currentPlayer === gameBoard[2]){
+        gameOver = true
+        document.getElementById("h2").innerHTML = `${currentPlayer.toUpperCase()}'s Wins!`
+    }else if(currentPlayer === gameBoard[2] && currentPlayer === gameBoard[5] && currentPlayer  === gameBoard[8]){
+        gameOver = true
+        document.getElementById("h2").innerHTML = `${currentPlayer.toUpperCase()}'s Wins!`
+    }else if(currentPlayer === gameBoard[0] && currentPlayer === gameBoard[4] && currentPlayer === gameBoard[8]){
+        gameOver = true
+        document.getElementById("h2").innerHTML = `${currentPlayer.toUpperCase()}'s Wins!`
+    }else if(currentPlayer === gameBoard[0] && currentPlayer === gameBoard[3] && currentPlayer === gameBoard[6]){
+        gameOver = true
+        document.getElementById("h2").innerHTML = `${currentPlayer.toUpperCase()}'s Wins!`
+    }else if(currentPlayer === gameBoard[1] && currentPlayer === gameBoard[4] && currentPlayer === gameBoard[7]){
+        gameOver = true
+        document.getElementById("h2").innerHTML = `${currentPlayer.toUpperCase()}'s Wins!`
+    }else if(currentPlayer === gameBoard[3] && currentPlayer === gameBoard[4] && currentPlayer === gameBoard[5]){
+        gameOver = true
+        document.getElementById("h2").innerHTML = `${currentPlayer.toUpperCase()}'s Wins!`
+    }else if(currentPlayer === gameBoard[6] && currentPlayer === gameBoard[7] && currentPlayer === gameBoard[8]){
+        gameOver = true
+        document.getElementById("h2").innerHTML = `${currentPlayer.toUpperCase()}'s Wins!`
+    }else if(currentPlayer === gameBoard[6] && currentPlayer === gameBoard[4] && currentPlayer === gameBoard[2]){
+        gameOver = true
+        document.getElementById("h2").innerHTML = `${currentPlayer.toUpperCase()}'s Wins!`
+    }
+}
+
 let playerMove = (player) => {
     if(gameOver !== true){
-        player.innerHTML = `${currentPlayer}`
-        changePlayers()
+        player.innerHTML = `${currentPlayer}`      
+    
     }
 }
 
 
 aOne.addEventListener('click', (event) => {
-    if(aOneSpace){
+    if(aOneSpace && gameOver !== true){
         playerMove(event.target)
         aOneSpace = false
+        gameBoard[0] = currentPlayer
+        winConditions()
+        changePlayers()
+      
     }
-    // console.log('click')
 })
 
 bOne.addEventListener('click', (event) => {
-    if(bOneSpace){
+    if(bOneSpace && gameOver !== true){
         playerMove(event.target)
         bOneSpace = false
+        gameBoard[1] = currentPlayer
+        winConditions()
+        changePlayers()
     }
-    // console.log('click')
+})
+
+cOne.addEventListener('click', (event) => {
+    if(cOneSpace && gameOver !== true){
+        playerMove(event.target)
+        cOneSpace = false
+        gameBoard[2] = currentPlayer
+        winConditions()
+        changePlayers()
+    }
+})
+aTwo.addEventListener('click', (event) => {
+    if(aTwoSpace && gameOver !== true){
+        playerMove(event.target)
+        aTwoSpace = false
+        gameBoard[3] = currentPlayer
+        winConditions()
+        changePlayers()
+    }
+})
+bTwo.addEventListener('click', (event) => {
+    if(bTwoSpace && gameOver !== true){
+        playerMove(event.target)
+        bTwoSpace = false
+        gameBoard[4] = currentPlayer
+        winConditions()
+        changePlayers()
+    }
+})
+cTwo.addEventListener('click', (event) => {
+    if(cTwoSpace && gameOver !== true){
+        playerMove(event.target)
+        cTwoSpace = false
+        gameBoard[5] = currentPlayer
+        winConditions()
+        changePlayers()
+    }
+})
+aThree.addEventListener('click', (event) => {
+    if(aThreeSpace && gameOver !== true){
+        playerMove(event.target)
+        aThreeSpace = false
+        gameBoard[6] = currentPlayer
+        winConditions()
+        changePlayers()
+    }
+})
+bThree.addEventListener('click', (event) => {
+    if(bThreeSpace && gameOver !== true){
+        playerMove(event.target)
+        bThreeSpace = false
+        gameBoard[7] = currentPlayer
+        winConditions()
+        changePlayers()
+    }
+})
+cThree.addEventListener('click', (event) => {
+    if(cThreeSpace && gameOver !== true){
+        playerMove(event.target)
+        cThreeSpace = false
+        gameBoard[8] = currentPlayer
+        winConditions()
+        changePlayers()
+    }
 })
 
 // some way to tell if a player has already clicked on a square

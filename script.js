e988d3b73364d03c19a4a08c7726ce7747efdc15
OmgValicious <ziallshamrock@gmail.com>
2022-08-13 18:45:32 -0700

    html and some css/js

---
+++ script.js
@@ -0,0 +1,14 @@
+// window.addEventListener('load', () => {
+//     function 
+// })
+
+let choice = document.querySelector('.box');
+
+choice.addEventListener("click", function() {
+   document.getElementsByClassName("box").innerHTML;
+ });
+
+ console.log("player chose" + choice)
+
+
+

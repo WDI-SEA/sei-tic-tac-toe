de1f170376b7beddb397a9dc93b4c270df417247
allent13 <allentang113@gmail.com>
2022-08-14 10:05:43 -0700

    adds ability to record moves to gameBoard array

--- script.js
+++ script.js
@@ -26,83 +26,119 @@
     // option hard mode: store all winning conditions and use loops to compare them to the gameBoard array
         // if the game is won, display the winner and prevent the users from clicking more
         // if it is a cats game, display that and prevent clicks
-let zero = document.getElementById('zero')
-let one = document.getElementById('one')
-let two = document.getElementById('two')
-let three = document.getElementById('three')
-let four = document.getElementById('four')
-let five = document.getElementById('five')
-let six = document.getElementById('six')
-let seven = document.getElementById('seven')
-let eight = document.getElementById('eight')
-let tiles = document.getElementsByClassName('tiles')
 
+// fetch elements
+const zero = document.getElementById('zero')
+const one = document.getElementById('one')
+const two = document.getElementById('two')
+const three = document.getElementById('three')
+const four = document.getElementById('four')
+const five = document.getElementById('five')
+const six = document.getElementById('six')
+const seven = document.getElementById('seven')
+const eight = document.getElementById('eight')
+const tiles = document.getElementsByClassName('tiles')
+const message = document.getElementById('message')
+
+// app state variables
 let turn = 'X'
-let moves = 9
-const gameBoard = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+let moves = 0
+let gameBoard = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
 let gameOver = false
 
-if (gameOver){
-    zero.removeEventListener('click', currentMove)
-    one.removeEventListener('click', currentMove)
-    two.removeEventListener('click', currentMove)
-    three.removeEventListener('click', currentMove)
-    four.removeEventListener('click', currentMove)
-    five.removeEventListener('click', currentMove)
-    six.removeEventListener('click', currentMove)
-    seven.removeEventListener('click', currentMove)
-    eight.removeEventListener('click', currentMove)
-}
-
+// logic for checking win conditions
 function checkWin() {
     if (gameBoard[0] == gameBoard[1] && gameBoard[1] == gameBoard[2]){
-
         gameOver = true
+        message.innerText = turn + "Wins!"
     }
     if (gameBoard[3] == gameBoard[4] && gameBoard[4] == gameBoard[5]){
-
         gameOver = true
+        message.innerText = turn + "Wins!"
     }
     if (gameBoard[6] == gameBoard[7] && gameBoard[7] == gameBoard[8]){
-
         gameOver = true
+        message.innerText = turn + "Wins!"
     }
     if (gameBoard[0] == gameBoard[3] && gameBoard[3] == gameBoard[6]){
-
         gameOver = true
+        message.innerText = turn + "Wins!"
     }
     if (gameBoard[1] == gameBoard[4] && gameBoard[4] == gameBoard[7]){
-
         gameOver = true
+        message.innerText = turn + "Wins!"
     }
     if (gameBoard[2] == gameBoard[5] && gameBoard[5] == gameBoard[8]){
-
         gameOver = true
+        message.innerText = turn + "Wins!"
     }
     if (gameBoard[0] == gameBoard[4] && gameBoard[4] == gameBoard[8]){
-
         gameOver = true
+        message.innerText = turn + "Wins!"
     }
     if (gameBoard[2] == gameBoard[4] && gameBoard[4] == gameBoard[6]){
-
         gameOver = true
+        message.innerText = turn + "Wins!"
     }
 }
 
+// function for player moves
 function currentMove(event){
-    if (moves == 9){
+    moves++
+    if (moves >= 9){
         gameOver = true
     }
+
     event.target.innerText = turn
+
+    // records moves to gameBoard array
+    if (event.target.id == 'zero'){
+        gameBoard[0] = turn
+    } else if (event.target.id == 'one'){
+        gameBoard[1] = turn
+    } else if (event.target.id == 'two'){
+        gameBoard[2] = turn
+    } else if (event.target.id == 'three'){
+        gameBoard[3] = turn
+    } else if (event.target.id == 'four'){
+        gameBoard[4] = turn
+    } else if (event.target.id == 'five'){
+        gameBoard[5] = turn
+    } else if (event.target.id == 'six'){
+        gameBoard[6] = turn
+    } else if (event.target.id == 'seven'){
+        gameBoard[7] = turn
+    } else {
+        gameBoard[8] = turn
+    }
+
+    checkWin()
+
+    // changes whos turn it is
     if (turn == 'X') {
         turn = 'O'
     }else {
         turn = 'X'
     }
-    moves++
-    console.log('clicked')
+    message.innerText = turn + "'s turn!"
+
+    console.log(moves)
+    console.log(gameBoard)
+    console.log(gameOver)
+    if (gameOver){
+        zero.removeEventListener('click', currentMove)
+        one.removeEventListener('click', currentMove)
+        two.removeEventListener('click', currentMove)
+        three.removeEventListener('click', currentMove)
+        four.removeEventListener('click', currentMove)
+        five.removeEventListener('click', currentMove)
+        six.removeEventListener('click', currentMove)
+        seven.removeEventListener('click', currentMove)
+        eight.removeEventListener('click', currentMove)
+    }
 }
 
+// adds event handlers to tile divs
 zero.addEventListener('click', currentMove, {once:true})
 one.addEventListener('click', currentMove, {once:true})
 two.addEventListener('click', currentMove, {once:true})
@@ -112,3 +148,4 @@ five.addEventListener('click', currentMove, {once:true})
 six.addEventListener('click', currentMove, {once:true})
 seven.addEventListener('click', currentMove, {once:true})
 eight.addEventListener('click', currentMove, {once:true})
+

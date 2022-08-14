d8191a55f169a818ec48ad5613dc87f1509c408f
allent13 <allentang113@gmail.com>
2022-08-13 21:43:39 -0700

    adds event handlers to tiles, removes them if game is over

--- script.js
+++ script.js
@@ -26,10 +26,33 @@
     // option hard mode: store all winning conditions and use loops to compare them to the gameBoard array
         // if the game is won, display the winner and prevent the users from clicking more
         // if it is a cats game, display that and prevent clicks
+let zero = document.getElementById('zero')
+let one = document.getElementById('one')
+let two = document.getElementById('two')
+let three = document.getElementById('three')
+let four = document.getElementById('four')
+let five = document.getElementById('five')
+let six = document.getElementById('six')
+let seven = document.getElementById('seven')
+let eight = document.getElementById('eight')
+let tiles = document.getElementsByClassName('tiles')
+
 let turn = 'X'
-let moves = 0
-const gameBoard = ['X', 'X', 'X', '', '', '', '', '', '']
+let moves = 9
+const gameBoard = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
 let gameOver = false
+
+if (gameOver){
+    zero.removeEventListener('click', currentMove)
+    one.removeEventListener('click', currentMove)
+    two.removeEventListener('click', currentMove)
+    three.removeEventListener('click', currentMove)
+    four.removeEventListener('click', currentMove)
+    five.removeEventListener('click', currentMove)
+    six.removeEventListener('click', currentMove)
+    seven.removeEventListener('click', currentMove)
+    eight.removeEventListener('click', currentMove)
+}
 
 function checkWin() {
     if (gameBoard[0] == gameBoard[1] && gameBoard[1] == gameBoard[2]){
@@ -61,8 +84,31 @@ function checkWin() {
         gameOver = true
     }
     if (gameBoard[2] == gameBoard[4] && gameBoard[4] == gameBoard[6]){
-        
+
         gameOver = true
     }
 }
-checkWin()
+
+function currentMove(event){
+    if (moves == 9){
+        gameOver = true
+    }
+    event.target.innerText = turn
+    if (turn == 'X') {
+        turn = 'O'
+    }else {
+        turn = 'X'
+    }
+    moves++
+    console.log('clicked')
+}
+
+zero.addEventListener('click', currentMove, {once:true})
+one.addEventListener('click', currentMove, {once:true})
+two.addEventListener('click', currentMove, {once:true})
+three.addEventListener('click', currentMove, {once:true})
+four.addEventListener('click', currentMove, {once:true})
+five.addEventListener('click', currentMove, {once:true})
+six.addEventListener('click', currentMove, {once:true})
+seven.addEventListener('click', currentMove, {once:true})
+eight.addEventListener('click', currentMove, {once:true})

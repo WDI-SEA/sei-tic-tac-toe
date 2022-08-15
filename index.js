79804f8da1efa6dcad30099a7cb962c50f4915c0
aqsakhatoon <khanabrar_02@yahoo.com>
2022-08-15 11:26:55 -0500

    aqsa's tic tac toe

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded',) () => {
+    const tiles = Array.from(document.querySelectorAll(.tile));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector ('#reset');
+    const announcer = document.querySelector('.announcer');
+    let board
+}

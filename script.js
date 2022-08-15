f857780007b89a88c896189d827e9d8a4076cfec
Moodybleu <meganbenn27@icloud.com>
2022-08-14 19:52:25 -0700

    rename div variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.getElementById(TopLeft)
+
+function Array2D(x, _o){
+    let arr = Array(x);
+     for(let i = 0; i < y; i++){
+      arr[i] = Array(y);
+     }
+     return arr;
+   }
+   console.log(Array2D([]))

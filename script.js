b1753b49083e83e8441ea759e0f92084af1ee589
OmgValicious <ziallshamrock@gmail.com>
2022-08-15 01:25:21 -0700

    some css/js

--- script.js
+++ script.js
@@ -1,14 +1,83 @@
+/* PSEUDOCODE (WEB FUNCTIONS):
+STEP 1: ALLOW PLAYER 1 TO GO FIRST (PLAYER ONE WILL ALWAYS BE Xs)
+STEP 2: WHEN BOX IS CLICKED THE PLAYERS MOVE SHOULD APPEAR ON BOX
+STEP 3: SWITH TO NEXT PLAYER AFTER MOVE HAS BEEN MADE
+STEP 4: STOP ALLOWING MOVES ONCE PLAYER WINS OR THERES A DRAW
+
+*/
+
+//GLOBAL VARs
+//makes element into array
+let choices = document.querySelectorAll('.box');
+choices = Array.from(choices)
+// console.log(choices);
+
+let plyOne = "X";
+
+let moves = 9;
+
+let gameActive = true;
+
+
+let winnerIf = [
+  //Horriz
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  //Vert
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+
+  //Diag
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+
 // window.addEventListener('load', () => {
 //     function 
 // })
 
-let choice = document.querySelector('.box');
 
-choice.addEventListener("click", function() {
-   document.getElementsByClassName("box").innerHTML;
- });
+// choice.addEventListener("click", function() {
+//    document.getElementsByClassName("box").innerHTML;
+//  });
 
- console.log("player chose" + choice)
+// gameBoard array to keep track of player moves ['x', '', 'o', 'x', '', 'o', 'x', '', '']
+    // 2d array
+    // [
+    //     ['x', '', 'o'],
+    //     ['x', 'o', ''],
+    //     ['x', '', 'o']
+    // ]
+
+
+
+
+ //Condtion to ensure it runs until no more moves available
+//  let plyMoves = 10;
+
+//  if (i = 9; i < plyMoves; i--) {
+//   console.log(i + " moves left")
+//  } else (i == plyMoves) {
+//    document.getElementsByTagName('section').style.pointerEvents = "none";
+//    alert("Oops, it's a draw! No more moves left.")
+//  };
+
+//  // condition to add X's || O's for players
+//  const playerOne = "X"
+//  const playerTwo = "O"
+
+//  while (playerOne) {
+
+//  }
+
+// Possible Choices
+
+//Draw/ end of game 
+
 
 
 

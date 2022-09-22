4f6169121a46704564e141f9d7dc6e1be73d8776
Anwaar <anwaaralsyouf@gmail.com>
2022-09-22 23:24:37 +0300

    style modification added

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Hello There")
+
+//Notes: organizing my thoughts hehe
+//each div should be clickable
+//when div is clickd, if odd then print X , if even print O
+//figure out all possiple winning conditions else it's a tie!
+//if odd print player turn, if even print player 2 turn

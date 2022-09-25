67fd2249da5bcc24d0d15143d2c8c311449016dc
FahadJanahi10 <fahad.a.janahi@gmail.com>
2022-09-25 09:09:39 +0300

    design only

---
+++ script.js
@@ -0,0 +1,5 @@
+const X = 'x'
+const O = 'o'
+const goal = [[0, 1, 2], [0, 4, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [3, 4, 5], [6, 7, 8]]
+const = document.getElementsByClassName('button');
+// my brain is really blank, I do not think I have the required skill-set to finish this exercise, not yet anyways.

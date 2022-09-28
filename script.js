e59b159135d732997b7f0247d9c6481b13f780c6
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2022-09-28 22:24:56 +0300

    Ahmed's Tic Tac Toe Deliverable Done

--- script.js
+++ script.js
@@ -1,297 +1,255 @@
-console.log('Hello World')
-
-let row1_col1_div = document.querySelector('#row-1-col-1-div')
-let row1_col2_div = document.querySelector('#row-1-col-2-div')
-let row1_col3_div = document.querySelector('#row-1-col-3-div')
-
-let row2_col1_div = document.querySelector('#row-2-col-1-div')
-let row2_col2_div = document.querySelector('#row-2-col-2-div')
-let row2_col3_div = document.querySelector('#row-2-col-3-div')
-
-let row3_col1_div = document.querySelector('#row-3-col-1-div')
-let row3_col2_div = document.querySelector('#row-3-col-2-div')
-let row3_col3_div = document.querySelector('#row-3-col-3-div')
-
-
-
-
 let game_divs = document.querySelectorAll('.game-square')
-
 let resetBtn = document.querySelector('.reset-btn')
 
+let resultDiv =  document.querySelector('.result-div')
+let result =  document.querySelector('.result-text')
 
 
-let player_1_turn =  document.querySelector('.player-1-turn')
-let player_2_turn =  document.querySelector('.player-2-turn')
-
-
+let player_1_turn =  document.querySelector('#player-1-turn-text')
+let player_2_turn =  document.querySelector('#player-2-turn-text')
 
 
 // This represnts the game
-// let gameTable = [
-//     [' ', ' ', ' '],
-//     [' ', ' ', ' '],
-//     [' ', ' ', ' '],
-// ]
+let gameTable = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+]
 
 
-
-let gameTable
 
 let stopGame = false
+let turnsCount = 0
+let currentPlayer = 'X'
+let winner 
 
 
 
+//will display the winner in the result div
+const displayWinner = () => {
 
-currentPlayer = 'X'
-
-
-//Then i will add if conditions to represent the winning conditions
-const checkTheWinner = (event) => {
-
-    //Horizontal
-    // let gameTable = [
-    //     ['X', 'X', 'X'],
-    //     ['O', 'O', 'X'],
-    //     ['X', 'X', 'X'],
-    // ]
-
-
-    for(let row=0; row<3; row++){
-
-
-        //Horizonal Line
-        if(gameTable[row][0] != ' ' && gameTable[row][0] == gameTable[row][1] && gameTable[row][1] == gameTable[row][2]){
-
-            console.log('Horizontal Line')
-
-
-            // Add style to that line
-
-
-            //end game
-            stopGame = True
-            return
-
-        }
+    if(winner === 'X'){
+        result.innerHTML = `${winner} Won!`
+    }else if (winner === 'O'){
+        result.innerHTML = `${winner} Won!`
+    }else{
+        result.innerHTML = 'It is a tie!'
     }
-
-            //Vertical Line
-            // let gameTable = [
-            //     ['X', 'O', 'X'],
-            //     ['X', 'O', 'X'],
-            //     ['X', 'O', 'X'],
-            // ]
-
-    for(let col=0; col<3; col++){
-            
-        if (gameTable[0][col] != ' ' &&  gameTable[0][col] == gameTable[1][col] && gameTable[1][col] == gameTable[2][col]) {
-
-            console.log('Vertical Line')
-
-            // Add style to that line
-
-
-            //end game
-            stopGame = True
-            return
-
-
-        }
-    }
-
-
-
-
-
-
-
-
-    //Diagonal
-    // let gameTable = [
-    //     ['O', 'O', 'X'],
-    //     ['O', 'X', 'O'],
-    //     ['X', 'O', 'O'],
-    // ]
-
-
-    //And
-    // let gameTable = [
-    //     ['X', 'O', 'O'],
-    //     ['O', 'X', 'O'],
-    //     ['O', 'O', 'X'],
-    // ]
-
-
-
-
+    
+    console.log(winner)
+    resultDiv.classList.remove('hidden')
 
 }
 
 
+const getWinner = () => {
+
+    if(currentPlayer === 'X'){
+        winner = 'O'   
+    }else{
+        winner = 'X' 
+    }
+}
 
 
+// check who is the winner
+const checkTheWinner = (event) => {
 
-const playGame  = (event) => {
+
+    // To find the horizontal win position "-"
+    for (let row = 0; row < 3; row++) {
+
+        if (gameTable[row][0] != ' ' && gameTable[row][0] == gameTable[row][1] && gameTable[row][1] == gameTable[row][2]) {
+
+            console.log('Horizontal Line')
+            getWinner()
+            displayWinner()
+
+            //end game
+            stopGame = true
+            return
+        }
+    }//end loop
 
 
+    // To find the vertical win position "|"
+    for (let col = 0; col < 3; col++) {
+        // check if table is not empty and that each values of column are the same
+        if (gameTable[0][col] != ' ' && gameTable[0][col] == gameTable[1][col] && gameTable[1][col] == gameTable[2][col]) {
 
-    if(stopGame) {
+            console.log('Vertical Line')
 
+            getWinner()
+            displayWinner()
+            //end game
+            stopGame = true
+            return
+        }
+    } //end loop
+
+
+    // To find the Diagonal win position "\"
+    if(gameTable[0][0] != ' ' &&  gameTable[0][0] === gameTable[1][1] &&  gameTable[1][1] === gameTable[2][2]){
+
+        console.log('Diagonal Line')
+        getWinner()
+        displayWinner()
+
+        //end game
+        stopGame = true
+        return
+
+    // To find the Diagonal win position "/"
+    } else if(gameTable[0][2] != ' ' &&  gameTable[0][2] === gameTable[1][1] &&  gameTable[1][1] === gameTable[2][0]){
+
+
+        getWinner()
+        displayWinner()
+
+        //end game
+        stopGame = true
+        return
+    }
+    
+
+
+    console.log(turnsCount)
+
+    // Game Tie Logic
+    if(turnsCount === 9 && stopGame===false){
+
+        console.log('It is a tie')
+        winner = ' '
+        displayWinner()
+
+        //end game
+        stopGame = true
+        return
+    }
+}
+
+
+const playGame = (event) => {
+
+    // Stop game if the it is true
+    if (stopGame) {
+
+        //Remove the click effect
+        for (let i = 0; i < game_divs.length; i++) {
+            game_divs[i].classList.add('disable')
+        }
         return
     }
 
+    // get id of the clicked element
     let clicked_div_id = event.target.id
 
+    // grab the element by id
     let divID = document.getElementById(clicked_div_id)
 
 
-    // console.log(clicked_div_id)
-    console.log(clicked_div_id)
+    // check if the clicked element is a square element and not an image
+    // image means that spot is taken
+    if (clicked_div_id.includes("img")) {
+        return
+    }
 
+    //Convert id to array position
     let div_arr = clicked_div_id.split('-')
-
-
-    console.log(div_arr)
-
 
     // -1 because the array indcies start from 0
     let row = parseInt(div_arr[1]) - 1
     let col = parseInt(div_arr[3]) - 1
 
-
-    console.log(row)
-    console.log(col)
-
-    console.log('Before Spot')
-
-    //Spot is taken
-    if(gameTable[row][col] != ' '){
-        console.log('Spot taken')
-        return
-    }
-
-
-    console.log('After Spot')
-
     //Modify the game table array
     gameTable[row][col] = currentPlayer
-
-
     console.log(gameTable)
 
-
-
-    //get the div's image
+    //In case if div is clicked then unhide the image and add disable effect
     let div_img = divID.children[0]
-
     div_img.classList.remove('hidden')
-
+    div_img.classList.add('disable')
 
 
     //This will allow to alternate between players
-
     // X player
-    if(currentPlayer == 'X'){
+    if (currentPlayer == 'X') {
 
         //add an image
         div_img.src = "images/x.png"
 
+        //modify the players turn css
+        player_1_turn.classList.remove('current-player')
+        player_1_turn.classList.add('other-player')
+        player_2_turn.classList.remove('other-player')
+        player_2_turn.classList.add('current-player')
+
         //this is the next player
         currentPlayer = 'O'
 
-
-    // O player
+        // O player
     } else {
         //add an image
         div_img.src = "images/o.png"
 
+        //modify the players turn css
+        player_2_turn.classList.remove('current-player')
+        player_2_turn.classList.add('other-player')
+        player_1_turn.classList.remove('other-player')
+        player_1_turn.classList.add('current-player')
+
+        //this is the next player
         currentPlayer = 'X'
     }
 
-    //Remove the hidden tag
-    // div_img.classList.remove('hidden')
+    turnsCount++
 
-
-
-
-    // //make if else to change between the images and the div of game turns
-
-    // div_img.src = "images/x.png"
-    // // console.log(div_img)
-
-
-    
     //See who wins
-
     checkTheWinner()
 }
 
-
-
-
 // Adding event listener to all the square
-
 const setDivsClick = () => {
 
+    for(let i=0; i<game_divs.length; i++){
+        // add an image when clicked
+        game_divs[i].addEventListener('click', playGame)
+    }
+}
 
+// Function to reset the game
+const resetGame = (event) => {
+
+    turnsCount = 0
+    stopGame = false
+
+    //Reset the game table
     gameTable = [
         [' ', ' ', ' '],
         [' ', ' ', ' '],
         [' ', ' ', ' '],
     ]
 
-
+    //Get image of a div then hide it and remove the disable mouse hover
     for(let i=0; i<game_divs.length; i++){
-    
-        let current_div = game_divs[i]
-    
-        // add an image when clicked
-        current_div.addEventListener('click', playGame)
-    
+        game_divs[i].children[0].classList.add('hidden')
+        game_divs[i].classList.remove('disable')
     }
 
+    //Add event listener click to the divs
+    setDivsClick()
 
-
-}
-
-
-
-
-
-const resetGame = (event) => {
-
-    for(let i=0; i<game_divs.length; i++){
-        let current_div = game_divs[i].children[0]
-        current_div.classList.add('hidden')
-
-    }
-
+    //Hide the result div
+    resultDiv.classList.add('hidden')
 }
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
-
-
-    //All the DOM Manipulation here
-
-
-
-
-
+    //Add Event listener to all divs
+    setDivsClick()
 
     //clear all the divs using the reset button
     resetBtn.addEventListener('click', resetGame)
-
-
-
-    //Add Event listener to div
-    setDivsClick()
-
-
-
-
-
 })

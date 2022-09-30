a00d8bb4827778e4e80c81c9f389256e173a0af0
MramRashidR <mramrashid98@gmail.com>
2022-09-30 19:54:10 +0300

    done but the winning / draw msg not workinggit add .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const cells = document.querySelectorAll(".dash")
-const messg = document.querySelectorAll(".msg")
-const restbutton = document.querySelectorAll(".style-button")
+const cells = document.querySelectorAll(".cell")
+const msg = document.querySelector(".msg")
+const restbutton = document.querySelector(".style-button")
 const winner = [
     [0, 1, 2],
     [3, 4, 5],
@@ -12,43 +12,81 @@ const winner = [
     [2, 4, 6],
 ];
 
-let choices = [" " , " " , " ", " ", " "," ", " ", " ", " " ]
+let choices = ["" , "" , "", "", "","", "", "", "" ];
 let currentPlyr = "X" ;
 let run = false;
 
+startGame();
 
 function startGame() {
-    cells.forEach(dash => dash.addEventListener("click" , chose))
-    restbutton.addEventListener("click" , resetGame)
-    
+    cells.forEach(cell => cell.addEventListener("click" , chose));
+    restbutton.addEventListener("click" , resetGame);
+    msg.textContent = `${currentPlyr}'s turn`;
+    run = true;
 }
 
 
 function chose() {
+    const cellIndex = this.getAttribute("cellIndex");
+    
+    if(choices[cellIndex] != "" || !run) {
+        return;
+    }
 
-
-
+    update(this , cellIndex);
+    changeRole();
+    winner();
 }
 
-function update() {
-
-
-
+function update(cell , index) {
+    choices[index] = currentPlyr;
+    cell.textContent = currentPlyr;
 }
+
 function changeRole () {
+    currentPlyr = (currentPlyr == "X") ? "O" : "X";
+    msg.textContent = `${currentPlyr}'s turn`;
 
 
 }
 
 function winning() {
+    let Win = false;
 
+    for(let i = 0; i < winner.length; i++) {
+        const cond =  winner[i] ;
+        const cellA = choices[cond[0]];
+        const cellB = choices[cond[1]];
+        const cellC = choices[cond[2]];
 
-}
+        if(cellA == "" || cellB == "" || cellC == "") {
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC ){
+            Win = true;
+            break;
+        }
+    }
+        if(Win) {
+            msg.textContent = `${currentPlyr}'s Wins!` ;
+            run = false ;
+        }
+        else if (!choices.includes("")) {
+            msg.textContent = "Draw!";
+            run = false;
+        }
+        else{
+            changeRole();
+        }
+    }
+
 
 
 function resetGame () {
-
-
-
+    currentPlyr = "X";
+    choices = ["" , "" , "", "", "","", "", "", "" ];
+    msg.textContent = `${currentPlyr}'s turn!` ;
+    cells.forEach(cell => cell.textContent = "");
+    run = true ;
 }
 

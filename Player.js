b7b2e839d16eec4bd7210b8abb07de45e5bc5d4f
jaywomack <jaredwomack@Jareds-MBP.lan>
2022-11-19 14:08:15 -0800

    Created Player class importing into app.js

---
+++ Player.js
@@ -0,0 +1,9 @@
+export default class Player {
+  constructor(name) {
+    this.name = name
+  }
+  sayHello() {
+    console.log(this.name)
+  }
+}
+// export class
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import Player from "./Player.js"
 // DOM SELECTORS
 const btn1 = document.getElementById("button1")
 const btn2 = document.getElementById("button2")
@@ -18,3 +19,29 @@ btn6.addEventListener("click", (e) => {})
 btn7.addEventListener("click", (e) => {})
 btn8.addEventListener("click", (e) => {})
 btn9.addEventListener("click", (e) => {})
+
+// Winning combinations and ties will be stored in an array or object
+// A game board array will store the game board
+const gameBoard = ["x", "o", "x", "o", "x", "o", "x", "o", "x"]
+
+// Start Game
+// A player is designated and the titles text content will display Player X's Turn: For ease of use the first player will always be x
+
+// player x will click a square on the board. The square will turn from the number to the letter x in the DOM.
+
+// A cell should not be able to be replayed once filled
+
+// An array will be populated with the game board and whichever square is clicked will be populated with either an X or an O depending on which players turn it is
+
+// Button should reset the board and start a new game
+
+// Winning combination idea. If a player clicks a square its coordinates within a 2d grid (x,y) are added to a "col array stored on a player object" For this idea to work the object would have to have 3 rows, 3 cols, and the two diagonals arrays. If any of these are full, or have a non empty array then the respective player has one.
+const Player1 = new Player("Jay")
+Player1.sayHello()
+
+/**
+ *    012
+ * 0{}{}{}
+ * 1{}{}{}
+ * 2{}{}{}
+ */

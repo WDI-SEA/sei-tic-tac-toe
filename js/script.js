c0a6068fca20e0c8f69eed8337c6732d1d5a7814
emilyjryan <emilyryanmac@gmail.com>
2022-11-20 07:32:14 -0800

    defining variables and laying out problem

---
+++ js/script.js
@@ -0,0 +1,21 @@
+console.log("we're linked yasss")
+
+// Variables
+
+let first = document.getElementById("first")
+let second = document.getElementById("second")
+let third = document.getElementById("third")
+
+let fourth = document.getElementById("fourth")
+let fifth = document.getElementById("fifth")
+let sixth = document.getElementById("sixth")
+
+let seventh = document.getElementById("seventh")
+let eighth = document.getElementById("eighth")
+let ninth = document.getElementById("ninth")
+
+let message = document.getElementsByClassName("message")
+let reset = document.getElementById("reset-btn")
+
+// element.addeventlistner('click', function-to-add-x-or-o)
+

15586221cbc9ccf88882f5b21e1d114d6c7ff85d
Gaymerbot <alexanderchmiel2@gmail.com>
2022-11-21 09:09:00 -0800

    tictactoe

---
+++ script.js
@@ -0,0 +1,7 @@
+//win con 
+//player 1 alternating to player 2
+//each grid square can only be used once
+//make in a tie or no win con
+//add event listeners to the grid
+//check win con after each move
+//add reset button

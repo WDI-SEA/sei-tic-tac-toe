2bf88cb9105af4eb5f125936a30d18101b764981
juanedcabrera <juanedcabrera@gmail.com>
2023-03-10 20:21:18 -0700

    Initial framework for everything

---
+++ script.js
@@ -0,0 +1,39 @@
+// Get from document
+name
+radio
+start button
+reset button
+display
+
+//create variables
+
+// event listener for radio
+
+// event listener for buttons
+
+// set current player
+
+// display current player
+
+// start timer
+
+// reset button
+ 
+
+// TIC TAC TOE
+// get from html
+const board = document.getElementsByClassName('board')
+const result = document.getElementsByClassName('result') 
+const reset = document.getElementsByClassName('reset')
+
+// array
+let gameBoard = [1,2,3,4,5,6,7,8,9]
+
+//event listener to grid
+    //check if cell is empty
+    //index of array
+    //update board
+    //check for win
+    //check for cats game
+    //disable board
+    //switch to other player

cf8402777af3e1f4e02edda2b6474ad3c845fdea
gloreea <aranda_gloree408@icloud.com>
2023-03-13 09:02:45 -0700

    Tic Tac Toe

---
+++ scripts.js
@@ -0,0 +1,38 @@
+const gridSquare = document.querySelector(".grid-container")
+const message = document.getElementById(".gameResults")
+const resetButton = document.querySelector("#resetButton")
+// let gridsquare = [" "," "," "," "," "," "," "," "," "]
+let playerOne = "X"
+let playerTwo = "O"
+// gridSquare.addEventListener("onclick", () => {})
+// // for (let i = 0; i < gridSquare.length; i++) {
+//     const square = gridSquare[i]
+//     square.addEventListener("onclick") 
+//     square.innerHTML="X" || "O"
+//     console.log(gridSquare)
+//}
+gridSquare.addEventListener("click", function () {
+    if (turn % 2 === 0) {
+        gridSquare.innerText = "O"
+    } else if (turn %2 !== 0) {
+        gridSquare.innerText = "X"
+    
+        console.log("x" || "O")
+    };
+})
+reset.addEventListener("click",function restartGame() {
+    gridSquare = [" "," "," "," "," "," "," "," "," "]
+})
+
+
+
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];

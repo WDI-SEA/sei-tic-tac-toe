6ddf4dbe3a533a2c28cecfdee80294cab9ace212
lvlivingston <lvlivingston@gmail.com>
2023-09-30 15:47:43 -0500

    file setup, confirming css & js connected

---
+++ js/main.js
@@ -0,0 +1,26 @@
+	/*----- constants -----*/
+
+
+
+    /*----- state variables -----*/
+
+// Decide State Variables for Game:
+// 1. Keep track of who's turn it is (turn: playerOne (1)/playerTwo (-1))
+// 2. Track board state (board: 2D array)
+//       null -> no player
+//       playerOne/playerTwo -> player at that cell   
+// 3. Variable for winner (winner: yes or no)
+//      null -> no winner / in play
+//      playerOne/playerTwo -> winner
+//      'T' -> tie  
+
+
+	/*----- cached elements  -----*/
+
+
+
+    /*----- event listeners -----*/
+
+
+
+    /*----- functions -----*/

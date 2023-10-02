1e303e19f56acaa545db6993568db74ac44c125e
kfac <kfacison1998@gmail.com>
2023-10-02 13:37:29 -0500

    added skelly for render

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*----- constants -----*/
+let playerOne;
+let playerTwo;
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+function render(){
+    clearBoard();
+    pickPlayer();
+}
+
+function pickPlayer(){
+
+}
+
+function clearBoard(){
+
+}

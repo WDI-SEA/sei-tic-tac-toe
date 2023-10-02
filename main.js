f0b853f2544ae0b382deb5e60674ec0b3ae9a349
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-10-01 17:42:12 -0700

    fixes renderBoard function

--- main.js
+++ main.js
@@ -21,19 +21,23 @@ console.log("hello, world!");
 /*----- cached elements  -----*/
     const messageEl = document.querySelector('h1');
     const playAgainBtn = document.querySelector('button');
+    // const squareEls = [...document.querySelectorAll('#board > div')];
 
 /*----- event listeners -----*/
     // Listen for a click in the board
     document.getElementById('board').addEventListener('click', handleFillSquare);
+    // Start over by calling init when clicked; DO NOT INVOKE
+    playAgainBtn.addEventListener('click', init);
 
 /*----- functions -----*/
     function init(){
         //initializing the let board var; rotate 90 deg counterclockwise to visualize the board in the DOM
         board = [
-            [1, 0, 0], // col 0
-            [-1, 0, 0], // col 1
-            [1, 0, 0], // col 2
+            [" ", " ", "o"], // col 0
+            [" ", "x", " "], // col 1
+            [" ", " ", " "], // col 2
         ]; 
+        console.log(board)
         //game begins with Player 1
         turn = 1;
         winner = null;
@@ -49,16 +53,33 @@ console.log("hello, world!");
 
     function renderBoard(){
         //Iterate through the board's array to determine what displays on the board
-        board.forEach(function(colArr, colIdx) {
-            // Iterate over the cells in the cur column (colArr)
-            colArr.forEach(function(cellVal, rowIdx) {
-                //Access the value in the col and row
-                const cellId = `c${colIdx}r${rowIdx}`;
+        board.forEach(function(colArr){
+            let colIdx = board.indexOf(colArr)
+            let rowIdx = -1;
+            colArr.forEach(function(rowArr){
+                rowIdx += 1;
+                console.log(`col row index ${colIdx}${rowIdx}`)
+                cellId = `c${colIdx}r${rowIdx}`
+                console.log(`cellid is ${cellId}`)
                 const cellEl = document.getElementById(cellId);
-                // UPDATE - DISPLAY TEXT, NOT COLOR
-                cellEl.style.backgroundColor = MOVES[cellVal];
-            });
-        });
+                cellEl.innerText = board[colIdx][rowIdx];
+            })
+        })
+
+
+        // let colIdx = -1;
+        // board.forEach(function(colArr) {
+        //     colIdx += 1;
+        //     let rowIdx = -1;
+        //     colArr.forEach(function(row){
+        //         console.log(row)
+        //         rowIdx += 1;
+        //         cellId = `c${colIdx}r${rowIdx}`
+        //         console.log(`cellid is ${cellId}`)
+        //         const cellEl = document.getElementById(cellId);
+        //         cellEl.innerText = board[colIdx][rowIdx];
+        //     })
+        // })
     }
 
     function renderMessage(){
@@ -67,7 +88,7 @@ console.log("hello, world!");
         }
         // Declare winner if truthy, but not T (tie)
         else if (winner) {
-            messageEl.innerHTML = `${MOVES[turn].toUpperCase()} won!`;
+            messageEl.innerHTML = `${MOVES[winner].toUpperCase()} won!`;
         }
         // Someone's turn
         else {
@@ -78,13 +99,84 @@ console.log("hello, world!");
 
     //In response to user interaction, update all impacted state then call render()
     function handleFillSquare(e){
-        const colIdx = markerEls.indexOf(e.target);
-        // Guards...
-        if (colIdx === -1) return;
-        const colArr = board[colIdx];
-        //Find the first index of the first 0 in colArr
-        const rowIdx = colArr.indexOf(0)
-        console.log(colIdx);
-
+        colIdx = 1
+        rowIdx = 1
+        // console.log(board[1])
+        // board[colIdx][rowIdx] = "x";
+        turn *= -1;
         render();
+
+        // console.log(e.target.id)
+        // const colIdx = substr(e.target.id);
+        // // Guards...
+        // if (colIdx === -1) return;
+        // const colArr = board[colIdx];
+        // //Find the first index of the first 0 in colArr -- change
+        // const rowIdx = colArr.indexOf(0)
+        // colArr[rowIdx] = turn;
+        // turn *= -1;
+        // winner = getWinner(colIdx, rowIdx)
+        // render();
     };
+
+    // Check for the winner in board state and return null if no winner
+    // Return 1/-1 if a player won, or T if tie
+    // RETURNING THE WRONG COLOR WINNER
+    function getWinner(colIdx, rowIdx) {
+        // Pass in the last play and where to begin counting to checkVW
+        return checkVerticalWin(colIdx, rowIdx) ||
+        checkHorizontalWin(colIdx, rowIdx) ||
+        checkDiagonalWinNESW(colIdx, rowIdx) ||
+        checkDiagonalWinNWSE(colIdx, rowIdx);
+    }
+
+    function checkDiagonalWinNESW (colIdx,rowIdx){
+        let adjCountNE = countAdjacent(colIdx, rowIdx, 1, 1);
+        let adjCountSW = countAdjacent(colIdx, rowIdx, -1, -1);
+        return (adjCountNE + adjCountSW) >= 3 ? board[colIdx][rowIdx] : null;
+    }
+
+    function checkDiagonalWinNWSE (colIdx,rowIdx){
+        let adjCountNW = countAdjacent(colIdx, rowIdx, 1, -1);
+        let adjCountSE = countAdjacent(colIdx, rowIdx, -1, 1);
+        return (adjCountNW + adjCountSE) >= 3 ? board[colIdx][rowIdx] : null;
+    }
+
+    // Check for horizontal win
+    function checkHorizontalWin (colIdx, rowIdx) {
+        let adjCountLeft = countAdjacent(colIdx, rowIdx, -1, 0);
+        let adjCountRight = countAdjacent(colIdx, rowIdx, 1, 0);
+        return (adjCountLeft + adjCountRight) >= 3 ? board[colIdx][rowIdx] : null;
+    }
+
+    // Check for vertical win (count the # of same elem in the col below last play)
+    function checkVerticalWin(colIdx, rowIdx){
+        return countAdjacent(colIdx, rowIdx, 0, -1) === 3 ? board[colIdx][rowIdx] : null;
+    }
+
+    function countAdjacent(colIdx, rowIdx, colOffset, rowOffset) {
+        // Shortcut var to play val's
+        const player = board[colIdx][rowIdx]
+        // Track count of adjacent cells with the same player val
+        let count = 0;
+        // Initialize colIdx and rowIdx locally with new values
+        colIdx += colOffset;
+        rowIdx += rowOffset;
+        // Create while loop to count
+        while (
+            // Ensure coldIdx is within bounds of the board array
+            board[colIdx] !== undefined &&
+            // Ensure rowIdx is within bounds of the board array
+            board[rowIdx] !== undefined &&
+            // cannot use board[colIdx][rowIdx] to check bc it returns error, not undefined if not existant
+            board[colIdx][rowIdx] === player
+        ) {
+            // If it meets all conditions, increment count
+            count++;
+            colIdx += colOffset;
+            rowIdx += rowOffset;
+        }
+        return count;
+    }
+
+    init();

46109e8deae917146845d159e2cd8ad67bf6f7bf
kfac <kfacison1998@gmail.com>
2023-10-05 00:08:43 -0500

    added cached, and funtions

--- js/main.js
+++ js/main.js
@@ -1,26 +1,83 @@
 /*----- constants -----*/
+const COLORS = {
+    "0": "white",
+    "1": "Blue",
+    "2": "Red"
+}
+
+/*----- state variables -----*/
+let borad;
+let turn; //1 blue or 2 red
+let winner; // null = no winner; 1 o 2 = winner; tie = T
 let playerOne;
 let playerTwo;
 
-/*----- state variables -----*/
-
-
 /*----- cached elements  -----*/
-
-
+const intructionsEl = document.getElementById("intructions");
+const resetBtnEl = document.getElementById("reset");
 /*----- event listeners -----*/
-
+document.getElementById("field").addEventListener("click", pickSpot);
 
 /*----- functions -----*/
+function init(){
+    borad =
+[
+[0,0,0],
+[0,0,0],
+[0,0,0],
+];
+//set players turn
+winner = null;
+render();
+}
+
 function render(){
     clearBoard();
+    renderMessage();
+    renderControls()
     pickPlayer();
+}
+
+function renderMessage(){
+    if(winner==="T"){
+        intructionsEl.innerText = "It's a Tie";
+    }
+    else if(winner === 1) {
+        intructionsEl.innerHTML = `<span style="color: ${COLORS[winner]};">${COLORS[winner].toUpperCase()}'s} Wins</span>`
+    } 
+    else {
+        intructionsEl.innerHTML = `<span style="color: ${COLORS[turn]};">${COLORS[turn].toUpperCase()}'s Turn</span>`
+    }
 }
 
 function pickPlayer(){
 
 }
+function renderControls(){
+    resetBtnEl.style.visibility = winner ? "visible": "hidden";
+}
 
 function clearBoard(){
-    
+    borad.forEach(function(colArr, colInx) {
+        colArr.forEach(function(cellVal,rowIdx) {
+            const cellId = `r${rowIdx}c${colInx}`;
+            const cellEl = document.getElementById(cellId);
+            cellEl.style.backgroundColor = COLORS[cellVal];
+        });
+    });
+}
+
+function pickSpot(evt){
+    //console.log(parseInt(evt.target.id.charAt(3),10));
+    borad[parseInt(evt.target.id.charAt(1),10)][parseInt(evt.target.id.charAt(3),10)] = turn;
+    let marker = document.createElement('p');
+    if(turn===1){
+    marker.innerHTML = "<strong>O</strong>"
+    marker.setAttribute("class",`p${turn}`)
+    }
+    else{
+    marker.innerHTML = "<strong>X</strong>"
+    }
+
+    render();
 }

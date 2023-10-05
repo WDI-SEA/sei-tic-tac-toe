d9de2cbf5ba053a8b9329d1e41c6eb8ff52d9f45
mselbekk11 <mselbekk11@gmail.com>
2023-10-05 12:02:54 -0700

    this is my latest tic tac toe after following a couple tutorials to learn how to do it. This first commit was my attempt

--- js/main.js
+++ js/main.js
@@ -1,59 +1,83 @@
-// script.js
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
 
-/*----- constants -----*/
-const COLORS = {
-  0: 'white',
-  1: 'X',
-  '-1': 'O',
+initializeGame();
+
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+}
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if(options[cellIndex] != "" || !running){
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+function checkWinner(){
+    let roundWon = false;
+
+    for(let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
+}
+function restartGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
 }
 
-/*----- state variables -----*/
-let board // array of 7 column arrays
-let turn // 1 or -1
-let winner // null = no winner; 1 or -1 = winner; 'T' = Tie
 
-/*----- cached elements  -----*/
-const messageEl = document.querySelector('h1')
-const playAgainBtn = document.querySelector('button')
-const cross = document.getElementsByClassName('x')
-// console.log(cross)
-const markerEls = [...document.querySelectorAll('#container > div')]
-// const markerEls = document.querySelectorAll('#container > div')
 
-/*----- event listeners -----*/
-document.getElementById('container').addEventListener('click', handleDrop)
-
-/*----- functions -----*/
-init()
-
-// Initialize all state, then call render()
-function init() {
-  // To visualize the board's mapping to the DOM,
-  // rotate the board array 90 degrees counter-clockwise
-  board = [
-    [0, 0, 0], // col 0
-    [0, 0, 0], // col 1
-    [0, 0, 0], // col 2
-  ]
-  turn = 1
-  winner = null
-  // render()
-}
-
-// function render() {
-//   renderBoard()
-//   renderMessage()
-//   renderControls()
-// }
-
-// function renderBoard() {}
-// function renderMessage() {}
-// function renderControls() {} // Hide/show UI elements (controls)
-
-function handleDrop(evt) {
-  const pointer = markerEls.indexOf(evt.target)
-  console.log(pointer)
-  // const cross = document.getElementsByClassName('x')
-  // console.log(cross)
-  // cross.style.visibility = 'block'
-}
